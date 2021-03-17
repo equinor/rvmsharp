86779980617392f688f08bd52089605f692ad3d2
Georgy Ushakov <gush@equinor.com>
2021-03-17 07:42:34 +0100

    Variable and class rename

--- RvmSharp/Tessellation/TessNet.cs
+++ RvmSharp/Tessellation/TessNet.cs
@@ -9,18 +9,18 @@ namespace RvmSharp.Tessellation
 {
     class TessNet
     {
-        public class JobData
+        public class TessellateResult
         {
             public Vector3[] VertexData = Array.Empty<Vector3>();
             public Vector3[] NormalData = Array.Empty<Vector3>();
             public List<int> Indices = new List<int>();
         }
 
-        public static JobData Tessellate(RvmContour[] contours)
+        public static TessellateResult Tessellate(RvmContour[] contours)
         {
-            var Job = new JobData();
-            var tess = new LibTessDotNet.Tess();
-            bool bContourFound = false;
+            var result = new TessellateResult();
+            var tess = new Tess();
+            bool shouldTessellate = false;
 
             foreach (var contour in contours)
             {
@@ -32,15 +32,14 @@ namespace RvmSharp.Tessellation
                 var cv = contour.Vertices.Select(v => new ContourVertex(new Vec3(v.v.X, v.v.Y, v.v.Z), v.n)).ToArray();
                 tess.AddContour(cv);
 
-                bContourFound = true;
+                shouldTessellate = true;
             }
 
-            if (bContourFound)
+            if (shouldTessellate)
             {
                 tess.Tessellate(WindingRule.EvenOdd, ElementType.Polygons, 3, VertexCombine);
-                int TessellatedVertexParameterCount = tess.VertexCount;
-                Job.VertexData = tess.Vertices.Select(v => new Vector3(v.Position.X, v.Position.Y, v.Position.Z)).ToArray();
-                Job.NormalData = tess.Vertices.Select(v => (Vector3)v.Data).ToArray();
+                result.VertexData = tess.Vertices.Select(v => new Vector3(v.Position.X, v.Position.Y, v.Position.Z)).ToArray();
+                result.NormalData = tess.Vertices.Select(v => (Vector3)v.Data).ToArray();
 
                 for (var i = 0; i < tess.ElementCount; i++)
                 {
@@ -48,16 +47,16 @@ namespace RvmSharp.Tessellation
                     Array.Copy(tess.Elements, i * 3, t, 0, 3);
                     if (t.Any(e => e == Tess.Undef))
                         continue;
-                    Job.Indices.AddRange(t);
+                    result.Indices.AddRange(t);
                 }
             }
-            return Job;
+            return result;
         }
 
-        private static object VertexCombine(Vec3 position, object[] data, float[] weights)
+        private static object VertexCombine(Vec3 position, object[] normals, float[] weights)
         {
             var max = weights.Select((w, i) => (w, i)).OrderByDescending(p => p.w).Select(p => p.i).First();
-            return data[max];
+            return normals[max];
         }
     }
 }
