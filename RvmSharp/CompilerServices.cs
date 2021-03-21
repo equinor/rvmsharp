25bebb64d213e5b794bd7f98e1a1114c7931cb25
Nils Henrik Hals <nih@equinor.com>
2021-03-21 10:42:23 +0100

    Comment why IsExternalInit is added

--- RvmSharp/CompilerServices.cs
+++ RvmSharp/CompilerServices.cs
@@ -3,6 +3,7 @@
 
 using System.ComponentModel;
 
+
 // ReSharper disable once CheckNamespace
 namespace System.Runtime.CompilerServices
 {
@@ -11,6 +12,19 @@ namespace System.Runtime.CompilerServices
     /// This class should not be used by developers in source code.
     /// </summary>
     [EditorBrowsable(EditorBrowsableState.Never)]
+
+    // Why is this added to RvmSharp?
+    // C# 9 added support for the init and record keywords. When using C# 9
+    // with target frameworks <= .NET 5.0, using these new features is not
+    // possible because the compiler is missing the IsExternalInit class,
+    // hence making the features unavailable for any target framework prior to .NET 5.
+    //
+    // Luckily, this problem can be solved by re-declaring the IsExternalInit
+    // class as an internal class in your own project. The compiler will use
+    // this custom class definition and thus allow you to use both the init
+    // keywords and records in any project.
+
+    // ReSharper disable once UnusedType.Global
     internal static class IsExternalInit
     {
     }
