810e2283796f4cb75e411f05f0f631afe7306124
Nils Henrik Hals <nih@equinor.com>
2021-05-25 17:31:19 +0200

    Add Real test

---
+++ CadRevealComposerTests/ProgramTests.cs
@@ -0,0 +1,34 @@
+namespace CadRevealComposerTests
+{
+    using CadRevealComposer;
+    using RvmSharp.Primitives;
+    using System.Numerics;
+    using NUnit.Framework;
+
+
+    [TestFixture]
+    public class ProgramTests
+    {
+        [Test]
+        public void CollectGeometryNodesRecursive_WhenGivenNodes_ExpandsHierarchyWithChildren()
+        {
+
+            var leaf = new RvmBox(2, Matrix4x4.Identity, new RvmBoundingBox() {Max = Vector3.One, Min = Vector3.Zero},
+                1, 1, 1);
+            
+            var node1 = new RvmNode(2, "Node1", Vector3.Zero, 0);
+            
+            node1.Children.Add(leaf);
+            var rvmRootNode = new RvmNode(2, "Root", Vector3.Zero, 0);
+            rvmRootNode.Children.Add(node1);
+
+            var rootNodeCadNode = CadRevealComposer.Program.CollectGeometryNodesRecursive(rvmRootNode, new CadNode());
+            
+            Assert.That(rootNodeCadNode.Children, Has.One.Items);
+            Assert.That(rootNodeCadNode.Children![0].Group, Is.EqualTo(node1));
+            
+            // Expecting that the leaf node not is included (yet).
+            Assert.That(rootNodeCadNode.Children[0].Children, Is.Null.Or.Empty);
+        }
+    }
+}
