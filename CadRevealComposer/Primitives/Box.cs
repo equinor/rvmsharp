936612d283a53475ca6ffb53ae1745f7feeb770f
Nils Henrik Hals <nih@equinor.com>
2021-05-27 11:58:38 +0200

    Remove extra NodeId and TreeIndex

--- CadRevealComposer/Primitives/Box.cs
+++ CadRevealComposer/Primitives/Box.cs
@@ -5,40 +5,24 @@ namespace CadRevealComposer
 
     public class Box : APrimitive
     {
-        [JsonProperty("node_id")]
-        public ulong NodeId { get; set; }
+        [JsonProperty("color")] public int[] Color { get; set; }
 
-        [JsonProperty("tree_index")]
-        public ulong TreeIndex { get; set; }
+        [JsonProperty("diagonal")] public float Diagonal { get; set; }
 
-        [JsonProperty("color")]
-        public int[] Color { get; set; }
+        [JsonProperty("center_x")] public float CenterX { get; set; }
 
-        [JsonProperty("diagonal")]
-        public float Diagonal { get; set; }
+        [JsonProperty("center_y")] public float CenterY { get; set; }
 
-        [JsonProperty("center_x")]
-        public float CenterX { get; set; }
+        [JsonProperty("center_z")] public float CenterZ { get; set; }
 
-        [JsonProperty("center_y")]
-        public float CenterY { get; set; }
+        [JsonProperty("normal")] public float[] Normal { get; set; }
 
-        [JsonProperty("center_z")]
-        public float CenterZ { get; set; }
+        [JsonProperty("delta_x")] public float DeltaX { get; set; }
 
-        [JsonProperty("normal")]
-        public float[] Normal { get; set; }
+        [JsonProperty("delta_y")] public float DeltaY { get; set; }
 
-        [JsonProperty("delta_x")]
-        public float DeltaX { get; set; }
+        [JsonProperty("delta_z")] public float DeltaZ { get; set; }
 
-        [JsonProperty("delta_y")]
-        public float DeltaY { get; set; }
-
-        [JsonProperty("delta_z")]
-        public float DeltaZ { get; set; }
-
-        [JsonProperty("rotation_angle")]
-        public float RotationAngle { get; set; }
+        [JsonProperty("rotation_angle")] public float RotationAngle { get; set; }
     }
 }
