29fdf112460bd5798c851cd6ad7c464cab23d269
Nils Henrik Hals <nih@equinor.com>
2021-05-31 16:15:39 +0200

    Create separate Exe project

---
+++ CadRevealComposer.Exe/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace CadRevealComposer.Exe
+{
+    public static class Program
+    {
+        // ReSharper disable once UnusedParameter.Local
+        static void Main(string[] args)
+        {
+            CadRevealComposer.Process(Options.InputRvmPath);
+        }
+    }
+}
