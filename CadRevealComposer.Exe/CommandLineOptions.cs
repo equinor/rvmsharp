7b11f12e2323abeb466002e0949b6cac34ae6060
Nils Henrik Hals <nih@equinor.com>
2021-05-31 20:18:42 +0200

    Fix CommandLineOptions.cs

--- CadRevealComposer.Exe/CommandLineOptions.cs
+++ CadRevealComposer.Exe/CommandLineOptions.cs
@@ -1,14 +1,16 @@
-﻿namespace CadRevealComposer
+﻿namespace CadRevealComposer.Exe
 {
     using CommandLine;
     using System.IO;
 
+    // ReSharper disable once ClassNeverInstantiated.Global - Its instantiated by CommandLineUtils NuGet Package
     public class CommandLineOptions
     {
-        [Option("InputDirectory", Required = true, HelpText = "The path to the RVM and .txt folder you want to convert")]
-        public DirectoryInfo InputDirectory {get;set;} = null!;
+        [Option("InputDirectory", Required = true,
+            HelpText = "The path to the RVM and .txt folder you want to convert")]
+        public DirectoryInfo InputDirectory { get; init; } = null!;
 
         [Option("OutputDirectory", Required = true, HelpText = "Where to position the output .i3df file(s)")]
-        public DirectoryInfo OutputDirectory { get; set; } = null!;
+        public DirectoryInfo OutputDirectory { get; init; } = null!;
     }
 }
