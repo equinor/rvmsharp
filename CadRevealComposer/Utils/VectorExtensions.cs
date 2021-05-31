b03a94ce35fd2967185233ae0af72f3c43b17d11
Nils Henrik Hals <nih@equinor.com>
2021-05-31 10:15:25 +0200

    Add Vector to Array extensions

---
+++ CadRevealComposer/Utils/VectorExtensions.cs
@@ -0,0 +1,27 @@
+﻿namespace CadRevealComposer.Utils
+{
+    using System.Numerics;
+
+    public static class VectorExtensions
+    {
+        /// <summary>
+        /// Copy the items to a new array of 3 items in XYZ order.
+        /// </summary>
+        public static float[] CopyToNewArray(this Vector3 vector3)
+        {
+            var floats = new float[3];
+            vector3.CopyTo(floats);
+            return floats;
+        }
+
+        /// <summary>
+        /// Copy the items to an Array of 4 items in XYZW order.
+        /// </summary>
+        public static float[] CopyToNewArray(this Vector4 vector4)
+        {
+            var floats = new float[4];
+            vector4.CopyTo(floats);
+            return floats;
+        }
+    }
+}
