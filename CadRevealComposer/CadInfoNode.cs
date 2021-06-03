5c8235b5a989ab66558b19fc44329abdc605ab86
Georgy Ushakov <gush@equinor.com>
2021-06-03 16:22:01 +0200

    Bugfix/rotation (#6)
    Collect data of all nodes
    Fixed rotation decomposition
    Reduced vector length threshold to allow greater precision on rotation at poles

---
+++ CadRevealComposer/CadInfoNode.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Numerics;
+
+public class CadInfoNode {
+    public ulong TreeIndex {get;set;}
+
+    public string Name {get;set;}
+
+    public List<CadGeometry> Geometries {get;set;}
+}
+
+public class CadGeometry
+{
+    public string TypeName;
+    public Vector3 Scale;
+    public Quaternion Rotation;
+    public Vector3 Location;
+    public Dictionary<string, string> Properties {get;set;}
+}
