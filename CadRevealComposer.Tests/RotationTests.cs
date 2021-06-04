c11a938d175b192987cd05c45828cb86fcdb4e4f
Georgy Ushakov <gush@equinor.com>
2021-06-04 12:51:23 +0200

    Clean up test definition

--- CadRevealComposer.Tests/RotationTests.cs
+++ CadRevealComposer.Tests/RotationTests.cs
@@ -4,43 +4,47 @@ namespace CadRevealComposer.Tests
     using Newtonsoft.Json;
     using NUnit.Framework;
     using System.IO;
+    using System.Linq;
     using System.Numerics;
 
     [TestFixture]
     public class RotationTests
     {
-        private readonly DirectoryInfo TestSamplesDirectory = new DirectoryInfo(Path.GetFullPath(Path.Join(TestContext.CurrentContext.TestDirectory, "TestSamples")));
+        private static readonly DirectoryInfo TestSamplesDirectory = new(Path.GetFullPath(Path.Join(TestContext.CurrentContext.TestDirectory, "TestSamples")));
         
-        public class RotationTestsJson
+        // ReSharper disable InconsistentNaming
+        // ReSharper disable UnassignedField.Global
+        // ReSharper disable ClassNeverInstantiated.Global
+        public class RotationTestCase
         {
-            public RotationTestCase[] testCases;
+            public string name;
+            public QuaternionSerializable quaternion;
+            public float answer;
 
-            public class RotationTestCase
+            public class QuaternionSerializable
             {
-                public QuaternionSerializable quaternion;
-                public float answer;
-
-                public class QuaternionSerializable
-                {
-                    public float x;
-                    public float y;
-                    public float z;
-                    public float w;
-                }
+                public float x;
+                public float y;
+                public float z;
+                public float w;
             }
         }
-        [Test]
-        public void CheckRotation1()
-        {
-            
-            var tests = JsonConvert.DeserializeObject<RotationTestsJson>(File.ReadAllText(Path.Combine(TestSamplesDirectory.FullName, "TestData.json")));
 
-            foreach (var test in tests.testCases)
-            {
-                var q = new Quaternion(test.quaternion.x,test.quaternion.y,test.quaternion.z,test.quaternion.w);
-                var components = q.DecomposeQuaternion();
-                Assert.AreEqual(components.rotationAngle, test.answer, 0.01f);
-            }
+        private static TestCaseData[] ReadTestCases()
+        {
+            var tests = JsonConvert.DeserializeObject<RotationTestCase[]>(File.ReadAllText(Path.Combine(TestSamplesDirectory.FullName, "TestData.json")));
+            return tests?.Select(x => new TestCaseData(x).SetName(x.name)).ToArray();
+        }
+
+        private static TestCaseData[] DivideCases => ReadTestCases();
+
+        [Test]
+        [TestCaseSource(nameof(DivideCases))]
+        public void CheckRotation1(RotationTestCase test)
+        {
+            var q = new Quaternion(test.quaternion.x,test.quaternion.y,test.quaternion.z,test.quaternion.w);
+            var components = q.DecomposeQuaternion();
+            Assert.AreEqual(components.rotationAngle, test.answer, 0.01f);
         }
     }
 }
