451fbb0d8d3e2d54e85ecbd9474daa3dc0e49a59
Nils Henrik Hals <nih@equinor.com>
2021-06-08 15:18:09 +0200

    Fix all nullable warnings

--- CadRevealComposer/CadInfoNode.cs
+++ CadRevealComposer/CadInfoNode.cs
@@ -9,18 +9,18 @@ namespace CadRevealComposer
     {
         public ulong TreeIndex { get; set; }
 
-        public string Name { get; set; }
+        public string? Name { get; set; }
 
-        public List<CadGeometry> Geometries { get; set; }
+        public List<CadGeometry>? Geometries { get; set; }
     }
 
     [JsonObject]
     public class CadGeometry
     {
-        public string TypeName;
+        public string? TypeName;
         public Vector3 Scale;
         public Quaternion Rotation;
         public Vector3 Location;
-        public Dictionary<string, string?> Properties { get; set; }
+        public Dictionary<string, string?>? Properties { get; set; }
     }
 }
