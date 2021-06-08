070ea8d7ad61558926a27494073d1490869e23c7
Nils Henrik Hals <nih@equinor.com>
2021-06-08 14:52:20 +0200

    Format RvmCylinder

--- RvmSharp/Primitives/RvmCylinder.cs
+++ RvmSharp/Primitives/RvmCylinder.cs
@@ -2,7 +2,11 @@ namespace RvmSharp.Primitives
 {
     using System.Numerics;
 
-    public record RvmCylinder(uint Version, Matrix4x4 Matrix, RvmBoundingBox BoundingBoxLocal, float Radius,
+    public record RvmCylinder(
+            uint Version,
+            Matrix4x4 Matrix,
+            RvmBoundingBox BoundingBoxLocal,
+            float Radius,
             float Height)
         : RvmPrimitive(Version, RvmPrimitiveKind.Cylinder, Matrix, BoundingBoxLocal);
 
