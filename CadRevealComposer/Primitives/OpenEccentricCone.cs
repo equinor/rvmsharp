f98fbc213fac43ee06dff9dc6ce267a198054923
Georgy Ushakov <gush@equinor.com>
2021-06-09 16:17:37 +0200

    Added eccentric cones, no conversion yet

---
+++ CadRevealComposer/Primitives/OpenEccentricCone.cs
@@ -0,0 +1,20 @@
+namespace CadRevealComposer.Primitives
+{
+    using Newtonsoft.Json;
+
+    public record OpenEccentricCone(
+        ulong NodeId,
+        ulong TreeIndex,
+        int[] Color,
+        float Diagonal,
+        float CenterX,
+        float CenterY,
+        float CenterZ,
+        [property: JsonProperty("center_axis")]
+        float[] CenterAxis,
+        [property: JsonProperty("height")] float Height,
+        [property: JsonProperty("radius_a")] float RadiusA,
+        [property: JsonProperty("radius_b")] float RadiusB,
+        [property: JsonProperty("cap_normal")] float[] CapNormal
+    ) : APrimitive(NodeId, TreeIndex, Color, Diagonal, CenterX, CenterY, CenterZ);
+}
