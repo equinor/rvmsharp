39db5806ba2689ce515f2972d96305ce8b0b5dbb
Nils Henrik Hals <nih@equinor.com>
2021-06-10 11:46:20 +0200

    Refactor constructors

--- CadRevealComposer/Primitives/OpenEccentricCone.cs
+++ CadRevealComposer/Primitives/OpenEccentricCone.cs
@@ -3,18 +3,12 @@ namespace CadRevealComposer.Primitives
     using Newtonsoft.Json;
 
     public record OpenEccentricCone(
-        ulong NodeId,
-        ulong TreeIndex,
-        int[] Color,
-        float Diagonal,
-        float CenterX,
-        float CenterY,
-        float CenterZ,
+        CommonPrimitiveProperties CommonPrimitiveProperties,
         [property: JsonProperty("center_axis")]
         float[] CenterAxis,
         [property: JsonProperty("height")] float Height,
         [property: JsonProperty("radius_a")] float RadiusA,
         [property: JsonProperty("radius_b")] float RadiusB,
         [property: JsonProperty("cap_normal")] float[] CapNormal
-    ) : APrimitive(NodeId, TreeIndex, Color, Diagonal, CenterX, CenterY, CenterZ);
+    ) : APrimitive(CommonPrimitiveProperties);
 }
