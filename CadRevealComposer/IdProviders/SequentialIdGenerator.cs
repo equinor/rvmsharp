60afd05a866b6027a45cab3403ce4a874195d283
Nils Henrik Hals <nih@equinor.com>
2021-06-11 15:43:39 +0200

    Add a SequentialIdGenerator

---
+++ CadRevealComposer/IdProviders/SequentialIdGenerator.cs
@@ -0,0 +1,22 @@
+﻿namespace CadRevealComposer.IdProviders
+{
+    using System;
+
+    public class SequentialIdGenerator
+    {
+        public const ulong MaxSafeInteger = (1L << 53) - 1;
+        
+        // ReSharper disable once RedundantDefaultMemberInitializer
+        private ulong _internalIdCounter = 0;
+
+        public ulong GetNextId()
+        {
+            var candidate = _internalIdCounter++;
+            if (candidate > MaxSafeInteger)
+                throw new Exception("Too many ids generated");
+            return candidate;
+        }
+
+        public ulong CurrentMaxGeneratedIndex => _internalIdCounter;
+    }
+}
