fd868a5d404ca97bea23bbe5bfce7ae057652bcd
Nils Henrik Hals <nih@equinor.com>
2021-06-11 15:43:56 +0200

    Convert TreeIndexGenerator to Sequential

--- CadRevealComposer/IdProviders/TreeIndexGenerator.cs
+++ CadRevealComposer/IdProviders/TreeIndexGenerator.cs
@@ -1,22 +1,8 @@
 namespace CadRevealComposer.IdProviders
 {
-    using System;
-
-    public class TreeIndexGenerator
+    public class TreeIndexGenerator : SequentialIdGenerator
     {
-        public const ulong MaxSafeInteger = (1L << 53) - 1;
-        
-        // ReSharper disable once RedundantDefaultMemberInitializer
-        private ulong _internalIdCounter = 0;
-
-        public ulong GetNextId()
-        {
-            var candidate = _internalIdCounter++;
-            if (candidate > MaxSafeInteger)
-                throw new Exception("Too many ids generated");
-            return candidate;
-        }
-
-        public ulong CurrentMaxGeneratedIndex => _internalIdCounter;
+        // TODO: The tree index generator should probably be something else. (To actually be a tree?)
+        //      For now its just a sequential generator.
     }
 }
