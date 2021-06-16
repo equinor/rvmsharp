22eddf811906c9f80065b9d8005f3e4a63e498ad
Nils Henrik Hals <nih@equinor.com>
2021-06-16 22:04:23 +0200

    Add WhereNotNull for parallel queries

--- CadRevealComposer.Tests/Utils/LinqExtensionsTests.cs
+++ CadRevealComposer.Tests/Utils/LinqExtensionsTests.cs
@@ -21,6 +21,15 @@
             Assert.That(result, Has.None.Null);
             Assert.That(result, Has.Exactly(2).Items);
         }
+        
+        [Test]
+        public void WhereNotNull_WithParallelClass_FiltersNullValues()
+        {
+            var sampleList = new List<TestDataClass> {new TestDataClass(), null, new TestDataClass(), null};
+            var result = sampleList.AsParallel().WithExecutionMode(ParallelExecutionMode.ForceParallelism).WhereNotNull().ToImmutableArray();
+            Assert.That(result, Has.None.Null);
+            Assert.That(result, Has.Exactly(2).Items);
+        }
 
         [Test]
         public void WhereNotNull_WithNullableStruct_FiltersNullValues()
@@ -38,6 +47,24 @@
             Assert.That(result, Has.None.Null);
             Assert.That(result, Has.Exactly(3).Items);
             Assert.That(result, Is.EqualTo(new[] {1, 2, 3}));
+        }
+        
+        [Test]
+        public void WhereNotNull_WithParallelNullableStruct_FiltersNullValues()
+        {
+            var sampleList = new List<int?>
+            {
+                1,
+                null,
+                2,
+                null,
+                3
+            };
+            var result = sampleList.AsParallel().WithExecutionMode(ParallelExecutionMode.ForceParallelism).WhereNotNull().ToArray();
+
+            Assert.That(result, Has.None.Null);
+            Assert.That(result, Has.Exactly(3).Items);
+            Assert.That(result, Is.EquivalentTo(new[] {1, 2, 3})); // Test for same elements, but not same sequence.
         }
     }
 }
