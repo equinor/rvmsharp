ee378c138176fcc8be265b1b25516a438416983a
Georgy Ushakov <gush@equinor.com>
2021-06-18 12:20:37 +0200

    Multiple warning fixes Added comment on float comparasion

--- CadRevealComposer.Tests/NodeIdProviderTests.cs
+++ CadRevealComposer.Tests/NodeIdProviderTests.cs
@@ -12,7 +12,7 @@
             // This test tests N random values. It should never fail.
             var nip = new NodeIdProvider();
 
-            const ulong maxSafeInt = TreeIndexGenerator.MaxSafeInteger;
+            const ulong maxSafeInt = SequentialIdGenerator.MaxSafeInteger;
 
             for(var i=0; i<100; i++)
                 Assert.That(nip.GetNodeId(null), Is.LessThanOrEqualTo(maxSafeInt));
