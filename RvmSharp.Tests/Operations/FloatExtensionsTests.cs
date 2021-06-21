fbe201f83e51060b781bec5d28047dfa2ca20046
Nils Henrik Hals <nih@equinor.com>
2021-06-21 16:01:05 +0200

    Improve FloatExtensions IsFinite, and add tests

---
+++ RvmSharp.Tests/Operations/FloatExtensionsTests.cs
@@ -0,0 +1,19 @@
+﻿namespace RvmSharp.Tests.Operations
+{
+    using NUnit.Framework;
+    using RvmSharp.Operations;
+
+    [TestFixture]
+    public class FloatExtensionsTests
+    {
+        [Test]
+        [TestCase(float.NaN, ExpectedResult = false)]
+        [TestCase(float.NegativeInfinity, ExpectedResult = false)]
+        [TestCase(float.PositiveInfinity, ExpectedResult = false)]
+        [TestCase(float.Epsilon, ExpectedResult = true)]
+        [TestCase(0, ExpectedResult = true)]
+        [TestCase(float.MaxValue, ExpectedResult = true)]
+        [TestCase(float.MinValue, ExpectedResult = true)]
+        public bool IsFiniteTests(float input) => input.IsFinite();
+    }
+}
