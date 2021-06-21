24efd116faaa727a46b9f76dfc91d4dd217951c3
Nils Henrik Hals <nih@equinor.com>
2021-06-21 16:01:05 +0200

    Improve FloatExtensions IsFinite, and add tests

--- RvmSharp/Operations/FloatExtensions.cs
+++ RvmSharp/Operations/FloatExtensions.cs
@@ -1,7 +1,12 @@
 ﻿namespace RvmSharp.Operations
 {
+    using System.Runtime.CompilerServices;
+
     public static class FloatExtensions
     {
+
+        /// <summary>Determines whether the specified value is finite (Not NaN && Not Infinity).</summary>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static bool IsFinite(this float f)
         {
             return !float.IsNaN(f) && !float.IsInfinity(f);
