5b2b7d07063f8b41ef97353b5d62018e04bf907c
Nils Henrik Hals <nih@equinor.com>
2021-06-23 17:58:59 +0200

    WIP: Pyramid templating

---
+++ CadRevealComposer.Tests/Primitives/Converters/RvmPyramidConverterTests.cs
@@ -0,0 +1,72 @@
+﻿namespace CadRevealComposer.Tests.Primitives.Converters
+{
+    using CadRevealComposer.Primitives;
+    using CadRevealComposer.Primitives.Converters;
+    using CadRevealComposer.Utils;
+    using NUnit.Framework;
+    using RvmSharp.Primitives;
+    using System.Numerics;
+
+    [TestFixture]
+    public class RvmPyramidConverterTests
+    {
+        private RvmPyramid _rvmPyramid;
+
+        private RvmNode _rvmNode;
+
+        const ulong nodeId = 675;
+        const int treeIndex = 1337;
+        private CadRevealNode _revealNode;
+
+
+        [SetUp]
+        public void Setup()
+        {
+            _rvmPyramid = new RvmPyramid(
+                Version: 2,
+                Matrix: Matrix4x4.Identity,
+                BoundingBoxLocal: new RvmBoundingBox(-Vector3.One, Vector3.One),
+                BottomX: 100,
+                BottomY: 50,
+                TopX: 50,
+                TopY: 25,
+                OffsetX: 0,
+                OffsetY: 0,
+                Height: 25
+            );
+            _rvmNode = new RvmNode(2, "BoxNode", new Vector3(1, 2, 3), 2);
+            _revealNode = new CadRevealNode() { NodeId = nodeId, TreeIndex = treeIndex };
+        }
+
+
+        [Test]
+        public void ConvertRvmPyramid_WhenTopAndBottomIsEqualAndNoOffset_IsBox()
+        {
+            const ulong nodeId = 675;
+            const int treeIndex = 1337;
+
+            var transform = Matrix4x4.Identity; // No rotation, scale 1, position at 0
+
+            var rvmBox = new RvmBox(Version: 2,
+                transform,
+                new RvmBoundingBox(new Vector3(-1, -2, -3), new Vector3(1, 2, 3)),
+                LengthX: 2, LengthY: 4, LengthZ: 6);
+            var rvmNode = new RvmNode(2, "BoxNode", new Vector3(1, 2, 3), 2);
+            var revealNode = new CadRevealNode() { NodeId = nodeId, TreeIndex = treeIndex };
+            var box = rvmBox.ConvertToRevealPrimitive(revealNode, rvmNode);
+
+            Assert.That(box, Is.Not.Null);
+            Assert.That(box, Is.TypeOf<Box>());
+            Assert.That(box.DeltaX, Is.EqualTo(2));
+            Assert.That(box.DeltaY, Is.EqualTo(4));
+            Assert.That(box.DeltaZ, Is.EqualTo(6));
+            Assert.That(box.CenterX, Is.EqualTo(0)); // Translation of RvmNode is ignored.
+            Assert.That(box.CenterY, Is.EqualTo(0));
+            Assert.That(box.CenterZ, Is.EqualTo(0));
+            Assert.That(box.NodeId, Is.EqualTo(nodeId));
+            Assert.That(box.TreeIndex, Is.EqualTo(treeIndex));
+            Assert.That(box.Normal, Is.EqualTo(Vector3.UnitZ));
+            Assert.That(box.RotationAngle, Is.EqualTo(0));
+        }
+    }
+}
---
+++ CadRevealComposer/Utils/PyramidConversionUtils.cs
@@ -0,0 +1,89 @@
+﻿namespace CadRevealComposer.Utils
+{
+    using RvmSharp.Primitives;
+    using RvmSharp.Tessellation;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class PyramidConversionUtils
+    {
+        private static void TessellatePyramids(IReadOnlyCollection<RvmNode> rvmNodes)
+        {
+            var pyramidsWithMeshes = rvmNodes
+                .AsParallel()
+                .Select(x =>
+                {
+                    const float tolerance = 0.01f; // Tolerance is ignored at the time of writing.
+                    var pyramidMeshes = x.Children.OfType<RvmPyramid>().Select(pyramid => (RvmPyramid: pyramid, Mesh: TessellatorBridge.Tessellate(pyramid, tolerance)));
+
+                    return pyramidMeshes;
+                });
+
+
+            bool almostEqual(float f1, float f2)
+            {
+                return MathF.Abs(f1 - f2) < 0.01;
+            }
+
+            var uniquePyramids = new Dictionary<RvmPyramid, int>();
+            var processedPyramids = new List<RvmPyramid>();
+            foreach (var rvmPyramid in rvmNodes.SelectMany(x => x.Children.OfType<RvmPyramid>())
+                .Select(CreatePyramidWithUnitSizeInXDimension))
+            {
+                var foundEqual = false;
+                foreach (var kvp in uniquePyramids)
+                {
+                    var uniquePyramid = kvp.Key;
+                    if ( /* almostEqual(rvmPyramid.Height, uniquePyramid.Height) */
+                        // We can scale for height, assuming the top is in same position
+                        (almostEqual(rvmPyramid.BottomX,
+                             uniquePyramid.BottomX) // BottomX should be equal for all, since they are scaled equal
+                         && almostEqual(rvmPyramid.BottomY, uniquePyramid.BottomY))
+                        && almostEqual(rvmPyramid.OffsetX, uniquePyramid.OffsetX)
+                        && almostEqual(rvmPyramid.OffsetY, uniquePyramid.OffsetY)
+                        && almostEqual(rvmPyramid.TopX, uniquePyramid.TopX)
+                        && almostEqual(rvmPyramid.TopY, uniquePyramid.TopY)
+                    )
+                    {
+                        uniquePyramids[uniquePyramid] = uniquePyramids[uniquePyramid] + 1;
+                        foundEqual = true;
+                        break;
+                    }
+                }
+
+                if (!foundEqual)
+                    uniquePyramids.Add(rvmPyramid, 0);
+
+
+                processedPyramids.Add(rvmPyramid);
+            }
+
+            Console.WriteLine("Pyramids");
+        }
+
+        public static RvmPyramid CreatePyramidWithUnitSizeInXDimension(RvmPyramid input)
+        {
+            if (input.BottomX < float.Epsilon || input.BottomY < float.Epsilon)
+            {
+                Console.WriteLine(input);
+            }
+
+            var unitScaleModifier = 1 / input.BottomX;
+
+            if (!float.IsFinite(unitScaleModifier * input.BottomX))
+                return input;
+
+            return input with
+            {
+                Height = input.Height * unitScaleModifier,
+                BottomX = input.BottomX * unitScaleModifier,
+                BottomY = input.BottomY * unitScaleModifier,
+                OffsetX = input.OffsetX * unitScaleModifier,
+                OffsetY = input.OffsetY * unitScaleModifier,
+                TopX = input.TopX * unitScaleModifier,
+                TopY = input.TopY * unitScaleModifier
+            };
+        }
+    }
+}
