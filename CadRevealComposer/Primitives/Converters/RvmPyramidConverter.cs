31008c0cc4bc33ca256daeda4ed6297b96ed6061
Nils Henrik Hals <nih@equinor.com>
2021-06-25 08:55:45 +0200

    Refactor

--- CadRevealComposer/Primitives/Converters/RvmPyramidConverter.cs
+++ CadRevealComposer/Primitives/Converters/RvmPyramidConverter.cs
@@ -8,7 +8,10 @@
 
     public static class RvmPyramidConverter
     {
-        public static APrimitive ConvertToRevealPrimitive(this RvmPyramid rvmPyramid, ulong meshFileId, CadRevealNode revealNode, RvmNode container)
+        public static APrimitive ConvertToRevealPrimitive(this RvmPyramid rvmPyramid,
+            ulong meshFileId,
+            CadRevealNode revealNode,
+            RvmNode container)
         {
             var commonProps = rvmPyramid.GetCommonProps(container, revealNode);
 
@@ -25,7 +28,7 @@
             else
             {
                 // TODO: Pyramids are a good candidate for instancing. Investigate how to best apply it.
-                var pyramidMesh = TessellatorBridge.Tessellate(rvmPyramid, tolerance: -1 /* Unused for pyramids */ );
+                var pyramidMesh = TessellatorBridge.Tessellate(rvmPyramid, tolerance: -1 /* Unused for pyramids */);
 
                 PrimitiveCounter.pyramid++;
                 if (pyramidMesh == null)
@@ -36,6 +39,10 @@
             }
         }
 
+        /// <summary>
+        /// Q: What is "Pyramid" that has an equal Top plane size to its bottom plane, and has no offset...
+        /// A: It is a box.
+        /// </summary>
         private static bool IsBoxShaped(RvmPyramid rvmPyramid)
         {
             const double tolerance = 0.01; // Arbitrary picked value
