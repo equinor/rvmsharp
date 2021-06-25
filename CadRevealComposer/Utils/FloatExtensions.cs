bfb2b61381087dd84fb2cf656a6d356b350cde75
Nils Henrik Hals <nih@equinor.com>
2021-06-25 08:41:12 +0200

    Create NearlyEquals float extension

---
+++ CadRevealComposer/Utils/FloatExtensions.cs
@@ -0,0 +1,22 @@
+﻿namespace CadRevealComposer.Utils
+{
+    using System;
+
+    public static class FloatExtensions
+    {
+        /// <summary>
+        /// Most our measurements are in meters. So this 0.00001 value gives us 1 micrometer leeway (usually the floats start having trouble here anyway).
+        /// </summary>
+        private const double DefaultTolerance = 0.00001f;
+
+        /// <summary>
+        /// Check if two floats are equal within a tolerance.
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <param name="tolerance">The tolerance </param>
+        /// <returns>True if nearly equal.</returns>
+        public static bool NearlyEquals(this float a, float b, double tolerance = DefaultTolerance)
+            => Math.Abs(a - b) < tolerance;
+    }
+}
