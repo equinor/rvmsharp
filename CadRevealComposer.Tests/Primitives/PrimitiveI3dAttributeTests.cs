b13d7985cc15540e3af87ea1c19e13dcdf3ce1b6
Nils Henrik Hals <nih@equinor.com>
2021-06-28 15:14:12 +0200

    Cleanup warning

--- CadRevealComposer.Tests/Primitives/PrimitiveI3dAttributeTests.cs
+++ CadRevealComposer.Tests/Primitives/PrimitiveI3dAttributeTests.cs
@@ -13,18 +13,18 @@
         [Test]
         public void CheckAllPrimitivesContainsOnlyI3dfProperties()
         {
-            var inheritedTypes = Assembly.GetAssembly(typeof(APrimitive))?.GetTypes()
+            var inheritedTypes = Assembly.GetAssembly(typeof(APrimitive))!.GetTypes()
                 .Where(t => t.IsClass && !t.IsAbstract && t.IsSubclassOf(typeof(APrimitive)));
-            
+
             foreach (Type type in inheritedTypes)
             {
                 var i3dfPropertiesCount = type.GetProperties().Count(p => p.GetCustomAttributes(true)
                     .Any(a => a is I3dfAttribute or JsonIgnoreAttribute));
                 var totalPropertiesCount = type.GetProperties().Length;
-                
+
                 Assert.That(i3dfPropertiesCount, Is.EqualTo(totalPropertiesCount), $"Type {type} is missing i3df attribute");
             }
         }
-        
+
     }
 }
