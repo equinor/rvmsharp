7ef0427ce828f73ce743b6df025576def53f4746
Nils Henrik Hals <nih@equinor.com>
2021-06-29 10:15:46 +0200

    Add DistinctBy Linq extension method

--- CadRevealComposer.Tests/Utils/LinqExtensionsTests.cs
+++ CadRevealComposer.Tests/Utils/LinqExtensionsTests.cs
@@ -16,16 +16,16 @@
         [Test]
         public void WhereNotNull_WithClass_FiltersNullValues()
         {
-            var sampleList = new List<TestDataClass> {new TestDataClass(), null, new TestDataClass(), null};
+            var sampleList = new List<TestDataClass> { new TestDataClass(), null, new TestDataClass(), null };
             var result = sampleList.WhereNotNull().ToImmutableArray();
             Assert.That(result, Has.None.Null);
             Assert.That(result, Has.Exactly(2).Items);
         }
-        
+
         [Test]
         public void WhereNotNull_WithParallelClass_FiltersNullValues()
         {
-            var sampleList = new List<TestDataClass> {new TestDataClass(), null, new TestDataClass(), null};
+            var sampleList = new List<TestDataClass> { new TestDataClass(), null, new TestDataClass(), null };
             var result = sampleList.AsParallel().WithExecutionMode(ParallelExecutionMode.ForceParallelism).WhereNotNull().ToImmutableArray();
             Assert.That(result, Has.None.Null);
             Assert.That(result, Has.Exactly(2).Items);
@@ -46,9 +46,9 @@
 
             Assert.That(result, Has.None.Null);
             Assert.That(result, Has.Exactly(3).Items);
-            Assert.That(result, Is.EqualTo(new[] {1, 2, 3}));
+            Assert.That(result, Is.EqualTo(new[] { 1, 2, 3 }));
         }
-        
+
         [Test]
         public void WhereNotNull_WithParallelNullableStruct_FiltersNullValues()
         {
@@ -64,7 +64,17 @@
 
             Assert.That(result, Has.None.Null);
             Assert.That(result, Has.Exactly(3).Items);
-            Assert.That(result, Is.EquivalentTo(new[] {1, 2, 3})); // Test for same elements, but not same sequence.
+            Assert.That(result, Is.EquivalentTo(new[] { 1, 2, 3 })); // Test for same elements, but not same sequence.
+        }
+
+        [Test]
+        public void DistinctBy_ReturnsDistinctValues()
+        {
+            var data = new[] { new { a = "A", b = "B" }, new { a = "B", b = "B" }, new { a = "A", b = "B" } };
+            var res = data.DistinctBy(x => x.a).ToArray();
+            Assert.That(res, Has.Exactly(2).Items);
+            var res2 = data.DistinctBy(x => x.b).ToArray();
+            Assert.That(res2, Has.Exactly(1).Items);
         }
     }
 }
