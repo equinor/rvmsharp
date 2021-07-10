a050f93bfaa4c39eed9e883cc0becde848093656
Nils Henrik Hals <nih@equinor.com>
2021-07-10 12:21:29 +0200

    Refactor Matrix4x4Helpers.cs

---
+++ RvmSharp/Operations/Matrix4x4Helpers.cs
@@ -0,0 +1,19 @@
+﻿namespace RvmSharp.Operations
+{
+    using System.Numerics;
+
+    // ReSharper disable once InconsistentNaming
+    public static class Matrix4x4Helpers
+    {
+        /// <summary>
+        /// Calculate a transform matrix based on position, rotation and scale.
+        /// Often named a TRS matrix (translation, rotation, scale).
+        /// </summary>
+        public static Matrix4x4 CalculateTransformMatrix(Vector3 position, Quaternion rotation, Vector3 scale)
+        {
+            return Matrix4x4.CreateScale(scale)
+                   * Matrix4x4.CreateFromQuaternion(rotation)
+                   * Matrix4x4.CreateTranslation(position);
+        }
+    }
+}
