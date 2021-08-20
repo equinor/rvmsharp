07353b58a958e035fdeed714b6212699a7ea2d4c
Nils Henrik Hals <nih@equinor.com>
2021-08-20 13:09:58 +0200

    Copy HierarchyComposer from MOP.Hierarchy

---
+++ HierarchyComposer/Model/NodePDMSEntry.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.SQLite;
+
+namespace Mop.Hierarchy.Model
+{
+    public class NodePDMSEntry
+    {
+        public uint NodeId { get; set; }
+        public Node Node { get; set; }
+
+        public long PDMSEntryId { get; set; }
+        public PDMSEntry PDMSEntry { get; set; }
+
+        public void RawInsert(SQLiteCommand command)
+        {
+            command.CommandText = "INSERT INTO NodeToPDMSEntry (NodeId, PDMSEntryId) VALUES (@NodeId, @PDMSEntryId);";
+            command.Parameters.AddRange(new[] {
+                    new SQLiteParameter("@NodeId", NodeId),
+                    new SQLiteParameter("@PDMSEntryId", PDMSEntryId)});
+            command.ExecuteNonQuery();
+        }
+    }
+}
