450215ffe4cbd9165ba63b38aa968cbb295d04ed
Nils Henrik Hals <nih@equinor.com>
2021-08-20 13:09:58 +0200

    Copy HierarchyComposer from MOP.Hierarchy

---
+++ HierarchyComposer/Model/PDMSEntry.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Mop.Hierarchy.Model
+{
+    public class PDMSEntry : IEquatable<PDMSEntry>
+    {
+        public long Id { get; set; }
+        public string Key { get; set; }
+        public string Value { get; set; }
+        public virtual ICollection<NodePDMSEntry> NodePDMSEntry { get; set; }
+
+        public bool Equals([AllowNull] PDMSEntry other)
+        {
+            //Check whether the compared object is null. 
+            if (ReferenceEquals(other, null)) return false;
+
+            //Check whether the compared object references the same data. 
+            if (ReferenceEquals(this, other)) return true;
+
+            return (Key.Equals(other.Key) && Value.Equals(other.Value));
+        }
+
+        public override int GetHashCode()
+        {
+            //Get hash code for the Name field if it is not null. 
+            int hashEntryKey = Key == null ? 0 : Key.GetHashCode();
+
+            //Get hash code for the Code field. 
+            int hashEntryValue = Value == null ? 0 : Value.GetHashCode();
+
+            //Calculate the hash code for the product. 
+            return hashEntryKey ^ hashEntryValue;
+        }
+
+        public void RawInsert(SQLiteCommand command)
+        {
+            command.CommandText = "INSERT INTO PDMSEntries (Id, Key, Value) VALUES (@Id, @Key, @Value);";
+            command.Parameters.AddRange(new[] {
+                        new SQLiteParameter("@Id", Id),
+                        new SQLiteParameter("@Key", Key),
+                        new SQLiteParameter("@Value", Value)});
+            command.ExecuteNonQuery();
+        }
+    }
+}
---
+++ HierarchyComposer/Model/Vector3f.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Mop.Hierarchy.Model
+{
+    [Owned]
+    public class Vector3f : IEquatable<Vector3f>
+    {
+        public float x { get; set; }
+        public float y { get; set; }
+        public float z { get; set; }
+
+        public bool Equals([AllowNull] Vector3f other)
+        {
+            //Check whether the compared object is null. 
+            if (Object.ReferenceEquals(other, null)) return false;
+
+            //Check whether the compared object references the same data. 
+            if (Object.ReferenceEquals(this, other)) return true;
+
+            //Check whether the products' properties are equal. 
+            return x == other.x && y == other.y && z == other.z;
+        }
+
+        public override int GetHashCode()
+        {
+            return x.GetHashCode() ^ y.GetHashCode() ^ z.GetHashCode();
+        }
+    }
+}
