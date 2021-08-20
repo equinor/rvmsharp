38f86433bd977348b3ef612c80671df85beaeb5e
Nils Henrik Hals <nih@equinor.com>
2021-08-20 18:04:35 +0200

    Add GetMaybeValue

---
+++ CadRevealComposer/Utils/DictionaryExtensions.cs
@@ -0,0 +1,17 @@
+﻿namespace CadRevealComposer.Utils
+{
+    using System.Collections.Generic;
+
+    public static class DictionaryExtensions
+    {
+        /// <summary>
+        /// Get the value assigned to the Key if the key exists, if not it returns null.
+        /// </summary>
+        public static TValue? GetMaybeValue<TKey, TValue>(this Dictionary<TKey, TValue> self, TKey key)
+            where TKey : notnull
+            where TValue : class // Remark: This does not work on non-reference types, so we just ignore it.
+        {
+            return self.TryGetValue(key, out var value) ? value : null;
+        }
+    }
+}
