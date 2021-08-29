355000dad932fcf547194149d7ec8e184542ea98
Georgy Ushakov <gush@equinor.com>
2021-08-29 22:42:56 +0200

    Fixed scale and rotation for most cases.
    Some cases will stilll result large error (few centimeters) during calculation

--- CadRevealComposer/Primitives/Instancing/RvmFacetGroupMatcher.cs
+++ CadRevealComposer/Primitives/Instancing/RvmFacetGroupMatcher.cs
@@ -1,9 +1,12 @@
 namespace CadRevealComposer.Primitives.Instancing
 {
+    using Newtonsoft.Json;
     using RvmSharp.Operations;
     using RvmSharp.Primitives;
     using System;
     using System.Diagnostics.CodeAnalysis;
+    using System.Drawing;
+    using System.IO;
     using System.Linq;
     using System.Numerics;
     using System.Runtime.CompilerServices;
@@ -170,33 +173,58 @@ namespace CadRevealComposer.Primitives.Instancing
             var vb13 = pb3 - pb1;
             var vb14 = pb4 - pb1;
 
+            var vaMatrix = new Matrix4x4(
+                va12.X * va12.X,va12.Y * va12.Y,va12.Z * va12.Z, 0,
+                va13.X * va13.X,va13.Y * va13.Y,va13.Z * va13.Z, 0,
+                va14.X * va14.X,va14.Y * va14.Y,va14.Z * va14.Z, 0,
+                0, 0, 0, 1);
+
+            var squaredBLengths = new Vector3(vb12.Length() * vb12.Length(), vb13.Length() * vb13.Length(), vb14.Length() * vb14.Length());
+            if (!Matrix4x4.Invert(vaMatrix, out var vaMatrixInverse))
+            {
+                throw new Exception("Could not invert matrix for scale");
+            }
+            var scale = new Vector3(
+                MathF.Sqrt(vaMatrixInverse.M11 * squaredBLengths.X + vaMatrixInverse.M12 * squaredBLengths.Y + vaMatrixInverse.M13 * squaredBLengths.Z),
+                MathF.Sqrt(vaMatrixInverse.M21 * squaredBLengths.X + vaMatrixInverse.M22 * squaredBLengths.Y + vaMatrixInverse.M23 * squaredBLengths.Z),
+                MathF.Sqrt(vaMatrixInverse.M31 * squaredBLengths.X + vaMatrixInverse.M32 * squaredBLengths.Y + vaMatrixInverse.M33 * squaredBLengths.Z));
+
+            va12 = va12 * scale;
+            va13 = va13 * scale;
+
             // 2 rotation va'1,va'2 -> vb1,vb2
-            var vaNormal = Vector3.Cross(Vector3.Normalize(va12), Vector3.Normalize(va13));
-            var vbNormal = Vector3.Cross(Vector3.Normalize(vb12), Vector3.Normalize(vb13));
-            var rot1 = vbNormal.FromToRotation(vaNormal);
+            var vaNormal = Vector3.Normalize(Vector3.Cross(Vector3.Normalize(va12), Vector3.Normalize(va13)));
+            var vbNormal = Vector3.Normalize(Vector3.Cross(Vector3.Normalize(vb12), Vector3.Normalize(vb13)));
+            var rot1 = vaNormal.FromToRotation(vbNormal);
 
             // 3 axis rotation: axis=vb2-vb1 va'3-va'1
             var va12r1 = Vector3.Transform(va12, rot1);
+            var va13r1 = Vector3.Transform(va13, rot1);
             var angle2 = va12r1.AngleTo(vb12);
-            var rot2 = Quaternion.CreateFromAxisAngle(vbNormal, angle2);
-            var rotation = rot1 * rot2;
+            var rotationNormal = Vector3.Normalize(Vector3.Cross(Vector3.Normalize(va12r1), Vector3.Normalize(vb12)));
+            var rot2 = Quaternion.CreateFromAxisAngle(rotationNormal, angle2);
+
+            var rotation = rot2 * rot1;
 
             // scale
             var va12rt = Vector3.Transform(va12, rotation);
             var va13rt = Vector3.Transform(va13, rotation);
-            var va14rt = Vector3.Transform(va14, rotation);
 
-            var scale12 = vb12 / va12rt;
-            var scale13 = vb13 / va13rt;
-            var scale14 = vb14 / va14rt;
+            var data = new VisData();
+            data.Arrows = new[]
+            {
+                new VisData.Arrow(Color.Red, pa1, va12),
+                new VisData.Arrow(Color.Red, pa1, va13),
+                new VisData.Arrow(Color.Blue, pb1, vb12),
+                new VisData.Arrow(Color.Blue, pb1, vb13),
+                new VisData.Arrow(Color.Pink, pa1, va12r1),
+                new VisData.Arrow(Color.Pink, pa1, va13r1),
+                new VisData.Arrow(Color.Green, pa1, va12rt),
+                new VisData.Arrow(Color.Green, pa1, va13rt),
+                //new VisData.Arrow(Color.Aqua, pb1, vbNormal)
+            };
+            File.WriteAllText("/Users/GUSH/projects/rvmsharp-vis/src/data.json", JsonConvert.SerializeObject(data, Formatting.Indented));
 
-            var array = new[] { scale12, scale13, scale14 };
-            // TODO: verify that First() always returns one item
-            var scale = new Vector3(
-                array.First(v => v.X.IsFinite()).X,
-                array.First(v => v.Y.IsFinite()).Y,
-                array.First(v => v.Z.IsFinite()).Z
-            );
 
             // translation
             var translation = pb1 - Vector3.Transform(pa1 * scale, rotation);
@@ -207,6 +235,9 @@ namespace CadRevealComposer.Primitives.Instancing
                 * Matrix4x4.CreateTranslation(translation);
             return true;
         }
+
+        public static Vector3 StoreEulers;
+        public static Vector3 StoreScale;
 
         /// <summary>
         /// Match a * transform = b
