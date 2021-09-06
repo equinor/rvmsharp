0b6fa70c070963c9f900ade23f16014f64740b00
Georgy Ushakov <gush@equinor.com>
2021-09-06 15:38:22 +0200

    Improved tests clarity

--- CadRevealComposer.Tests/FacetGroupMatchTests.cs
+++ CadRevealComposer.Tests/FacetGroupMatchTests.cs
@@ -3,101 +3,53 @@
     using CadRevealComposer.Primitives.Instancing;
     using Newtonsoft.Json;
     using NUnit.Framework;
-    using RvmSharp.Exporters;
     using RvmSharp.Primitives;
-    using RvmSharp.Tessellation;
     using System.IO;
+    using Utils;
 
     [TestFixture]
     public class FacetGroupMatchTests
     {
-        private static readonly string TestSamplesDirectory = Path.GetFullPath(Path.Join(TestContext.CurrentContext.TestDirectory, "TestSamples"));
-
-        private static T LoadTestJson<T>(string filename)
+        [Test]
+        public void MatchTwoBentPipes()
         {
-            return JsonConvert.DeserializeObject<T>(File.ReadAllText(Path.Combine(TestSamplesDirectory, filename)));
+            var pipe1 = DataLoader.LoadTestJson<RvmFacetGroup>("43907.json");
+            var pipe2 = DataLoader.LoadTestJson<RvmFacetGroup>("43908.json");
+            var pipesEqual = RvmFacetGroupMatcher.Match(pipe1, pipe2, out var transform);
+            Assert.That(pipesEqual);
         }
 
         [Test]
-        public void TestPipes()
+        public void MatchRotatedHinges()
+        {
+            var hinges1 = JsonConvert.DeserializeObject<RvmFacetGroup>(
+                File.ReadAllText(Path.Combine(DataLoader.TestSamplesDirectory, "m1.json")));
+            var hinges2 = JsonConvert.DeserializeObject<RvmFacetGroup>(
+                File.ReadAllText(Path.Combine(DataLoader.TestSamplesDirectory, "m2.json")));
+            var hingesEqual = RvmFacetGroupMatcher.Match(hinges1, hinges2, out var transform);
+            Assert.IsFalse(hingesEqual);
+        }
+
+        [Test]
+        public void MatchUnequalPanelsWithOffset()
+        {
+            var panel1 = JsonConvert.DeserializeObject<RvmFacetGroup>(
+                File.ReadAllText(Path.Combine(DataLoader.TestSamplesDirectory, "0.json")));
+            var panel2 = JsonConvert.DeserializeObject<RvmFacetGroup>(
+                File.ReadAllText(Path.Combine(DataLoader.TestSamplesDirectory, "2.json")));
+            var panelsEqual = RvmFacetGroupMatcher.Match(panel1, panel2, out var transform);
+            Assert.IsFalse(panelsEqual);
+        }
+
+        [Test]
+        public void MatchEqualPanelsWithDifferentPolygonOrder()
         {
             var pipe1 = JsonConvert.DeserializeObject<RvmFacetGroup>(
-                File.ReadAllText(Path.Combine(TestSamplesDirectory, "43907.json")));
+                File.ReadAllText(Path.Combine(DataLoader.TestSamplesDirectory, "5.json")));
             var pipe2 = JsonConvert.DeserializeObject<RvmFacetGroup>(
-                File.ReadAllText(Path.Combine(TestSamplesDirectory, "43907.json")));
+                File.ReadAllText(Path.Combine(DataLoader.TestSamplesDirectory, "6.json")));
             var facetGroupsEqual = RvmFacetGroupMatcher.Match(pipe1, pipe2, out var transform);
             Assert.That(facetGroupsEqual);
         }
-
-        [Test]
-        public void TestPipes2()
-        {
-            var pipe1 = JsonConvert.DeserializeObject<RvmFacetGroup>(
-                File.ReadAllText(Path.Combine(TestSamplesDirectory, "m1.json")));
-            var pipe2 = JsonConvert.DeserializeObject<RvmFacetGroup>(
-                File.ReadAllText(Path.Combine(TestSamplesDirectory, "m2.json")));
-            var facetGroupsEqual = RvmFacetGroupMatcher.Match(pipe1, pipe2, out var transform);
-            Assert.IsFalse(facetGroupsEqual);
-        }
-
-        [Test]
-        public void TestPipes3()
-        {
-            var pipe1 = JsonConvert.DeserializeObject<RvmFacetGroup>(
-                File.ReadAllText(Path.Combine(TestSamplesDirectory, "0.json")));
-            var pipe2 = JsonConvert.DeserializeObject<RvmFacetGroup>(
-                File.ReadAllText(Path.Combine(TestSamplesDirectory, "2.json")));
-            var facetGroupsEqual = RvmFacetGroupMatcher.Match(pipe1, pipe2, out var transform);
-            Assert.IsFalse(facetGroupsEqual);
-        }
-
-        [Test]
-        public void TestPipes4()
-        {
-            var pipe1 = JsonConvert.DeserializeObject<RvmFacetGroup>(
-                File.ReadAllText(Path.Combine(TestSamplesDirectory, "5.json")));
-            var pipe2 = JsonConvert.DeserializeObject<RvmFacetGroup>(
-                File.ReadAllText(Path.Combine(TestSamplesDirectory, "6.json")));
-            var facetGroupsEqual = RvmFacetGroupMatcher.Match(pipe1, pipe2, out var transform);
-            Assert.That(facetGroupsEqual);
-        }
-
-        [Explicit]
-        [Test]
-        public void WriteObj()
-        {
-            var f1 = LoadTestJson<RvmFacetGroup>("m1.json");
-            var f2 = LoadTestJson<RvmFacetGroup>("m2.json");
-            var objExporter = new ObjExporter("D:\\m.obj");
-            objExporter.StartGroup("m1");
-            objExporter.WriteMesh(TessellatorBridge.Tessellate(f1, 5.0f));
-            objExporter.StartGroup("m2");
-            objExporter.WriteMesh(TessellatorBridge.Tessellate(f2, 5.0f));
-        }
-
-        [Explicit]
-        [Test]
-        public void WriteObj2()
-        {
-            var f1 = LoadTestJson<RvmFacetGroup>("5.json");
-            var f2 = LoadTestJson<RvmFacetGroup>("6.json");
-            using var objExporter = new ObjExporter("D:\\m.obj");
-
-            for (var i = 0; i < f1.Polygons.Length; i++)
-            {
-                var p = f1.Polygons[i];
-                var m1 = f1 with { Polygons = new[] { p } };
-                objExporter.StartGroup($"m5_p{i}");
-                objExporter.WriteMesh(TessellatorBridge.Tessellate(m1, 5.0f));
-            }
-            for (var i = 0; i < f2.Polygons.Length; i++)
-            {
-                var p = f2.Polygons[i];
-                var m1 = f2 with { Polygons = new[] { p } };
-                objExporter.StartGroup($"m6_p{i}");
-                objExporter.WriteMesh(TessellatorBridge.Tessellate(m1, 5.0f));
-            }
-        }
-
     }
 }
---
+++ CadRevealComposer.Tests/Utils/FacetGroupHelpers.cs
@@ -0,0 +1,61 @@
+﻿namespace CadRevealComposer.Tests.Utils
+{
+    using NUnit.Framework;
+    using RvmSharp.Exporters;
+    using RvmSharp.Primitives;
+    using RvmSharp.Tessellation;
+    using System.IO;
+
+    /// <summary>
+    /// Use methods provided here to manually extract and inspect data
+    /// </summary>
+    [TestFixture]
+    public class FacetGroupHelpers
+    {
+        private const string ExportPath = "D:\\tmp";
+        private static readonly string[] FacetGroupJsons = new[]
+        {
+            "m1.json",
+            "m2.json",
+            "5.json",
+            "6.json"
+        };
+
+        [Explicit]
+        [Test]
+        public void ExportAsObj()
+        {
+            Directory.CreateDirectory(ExportPath);
+            foreach (var facetGroupJson in FacetGroupJsons)
+            {
+                var facetGroup = DataLoader.LoadTestJson<RvmFacetGroup>(facetGroupJson);
+                var name = facetGroupJson.Replace(".json", "");
+                using var objExporter = new ObjExporter(Path.Combine(ExportPath, $"{name}.obj"));
+                objExporter.StartGroup(name);
+                objExporter.WriteMesh(TessellatorBridge.Tessellate(facetGroup, 5.0f));
+            }
+        }
+
+        [Explicit]
+        [Test]
+        public void ExportPolygonsAsObj()
+        {
+            Directory.CreateDirectory(ExportPath);
+            foreach (var facetGroupJson in FacetGroupJsons)
+            {
+                var facetGroup = DataLoader.LoadTestJson<RvmFacetGroup>(facetGroupJson);
+                var name = facetGroupJson.Replace(".json", "");
+                using var objExporter = new ObjExporter(Path.Combine(ExportPath, $"{name}_polys.obj"));
+                objExporter.StartGroup(name);
+
+                for (var i = 0; i < facetGroup.Polygons.Length; i++)
+                {
+                    var p = facetGroup.Polygons[i];
+                    var m1 = facetGroup with { Polygons = new[] { p } };
+                    objExporter.StartGroup($"{name}_p{i}");
+                    objExporter.WriteMesh(TessellatorBridge.Tessellate(m1, 5.0f));
+                }
+            }
+        }
+    }
+}
