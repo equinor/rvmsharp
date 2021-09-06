0c0f0d4d71fd563330ffb1ffe9a7c9c0fdbcdba2
Georgy Ushakov <gush@equinor.com>
2021-09-06 15:38:22 +0200

    Improved tests clarity

---
+++ CadRevealComposer.Tests/Utils/DataLoader.cs
@@ -0,0 +1,16 @@
+﻿namespace CadRevealComposer.Tests.Utils
+{
+    using Newtonsoft.Json;
+    using NUnit.Framework;
+    using System.IO;
+
+    public class DataLoader
+    {
+        public static readonly string TestSamplesDirectory = Path.GetFullPath(Path.Join(TestContext.CurrentContext.TestDirectory, "TestSamples"));
+
+        public static T LoadTestJson<T>(string filename)
+        {
+            return JsonConvert.DeserializeObject<T>(File.ReadAllText(Path.Combine(TestSamplesDirectory, filename)));
+        }
+    }
+}
