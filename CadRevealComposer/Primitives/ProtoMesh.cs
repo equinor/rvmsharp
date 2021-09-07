275b893ec340e5301e486f09839055001e130a7a
Georgy Ushakov <gush@equinor.com>
2021-09-07 13:49:31 +0200

    Added description for Protomesh

--- CadRevealComposer/Primitives/ProtoMesh.cs
+++ CadRevealComposer/Primitives/ProtoMesh.cs
@@ -2,6 +2,12 @@
 {
     using RvmSharp.Primitives;
 
+    /// <summary>
+    /// This mesh will evolve into either InstancedMesh or TriangleMesh depending on multiple factors
+    /// like use count, position and sector allocation
+    /// </summary>
+    /// <param name="CommonPrimitiveProperties"></param>
+    /// <param name="SourceMesh">Facet group the mesh is based on</param>
     public record ProtoMesh(CommonPrimitiveProperties CommonPrimitiveProperties,
         RvmFacetGroup SourceMesh
         ) : APrimitive(CommonPrimitiveProperties)
