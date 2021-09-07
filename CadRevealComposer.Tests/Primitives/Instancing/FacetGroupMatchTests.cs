6e2d20c79f7b202a1b61c16a39975327d8fff200
Georgy Ushakov <gush@equinor.com>
2021-09-07 15:33:01 +0200

    Renamed DataLoader to TestSampleLoader

--- CadRevealComposer.Tests/Primitives/Instancing/FacetGroupMatchTests.cs
+++ CadRevealComposer.Tests/Primitives/Instancing/FacetGroupMatchTests.cs
@@ -13,7 +13,7 @@
         [Test]
         public void MatchItself()
         {
-            var pipe1 = DataLoader.LoadTestJson<RvmFacetGroup>("43907.json");
+            var pipe1 = TestSampleLoader.LoadTestJson<RvmFacetGroup>("43907.json");
             var pipesEqual = RvmFacetGroupMatcher.Match(pipe1, pipe1, out var transform);
             Assert.That(pipesEqual);
         }
@@ -21,8 +21,8 @@
         [Test]
         public void MatchTwoBentPipes()
         {
-            var pipe1 = DataLoader.LoadTestJson<RvmFacetGroup>("43907.json");
-            var pipe2 = DataLoader.LoadTestJson<RvmFacetGroup>("43908.json");
+            var pipe1 = TestSampleLoader.LoadTestJson<RvmFacetGroup>("43907.json");
+            var pipe2 = TestSampleLoader.LoadTestJson<RvmFacetGroup>("43908.json");
             var pipesEqual = RvmFacetGroupMatcher.Match(pipe1, pipe2, out var transform);
             Assert.That(pipesEqual);
         }
@@ -31,9 +31,9 @@
         public void MatchRotatedHinges()
         {
             var hinges1 = JsonConvert.DeserializeObject<RvmFacetGroup>(
-                File.ReadAllText(Path.Combine(DataLoader.TestSamplesDirectory, "m1.json")));
+                File.ReadAllText(Path.Combine(TestSampleLoader.TestSamplesDirectory, "m1.json")));
             var hinges2 = JsonConvert.DeserializeObject<RvmFacetGroup>(
-                File.ReadAllText(Path.Combine(DataLoader.TestSamplesDirectory, "m2.json")));
+                File.ReadAllText(Path.Combine(TestSampleLoader.TestSamplesDirectory, "m2.json")));
             var hingesEqual = RvmFacetGroupMatcher.Match(hinges1, hinges2, out var transform);
             Assert.IsFalse(hingesEqual);
         }
@@ -42,9 +42,9 @@
         public void MatchUnequalPanelsWithOffset()
         {
             var panel1 = JsonConvert.DeserializeObject<RvmFacetGroup>(
-                File.ReadAllText(Path.Combine(DataLoader.TestSamplesDirectory, "0.json")));
+                File.ReadAllText(Path.Combine(TestSampleLoader.TestSamplesDirectory, "0.json")));
             var panel2 = JsonConvert.DeserializeObject<RvmFacetGroup>(
-                File.ReadAllText(Path.Combine(DataLoader.TestSamplesDirectory, "2.json")));
+                File.ReadAllText(Path.Combine(TestSampleLoader.TestSamplesDirectory, "2.json")));
             var panelsEqual = RvmFacetGroupMatcher.Match(panel1, panel2, out var transform);
             Assert.IsFalse(panelsEqual);
         }
@@ -58,9 +58,9 @@
         public void MatchEqualPanelsWithDifferentPolygonOrder()
         {
             var pipe1 = JsonConvert.DeserializeObject<RvmFacetGroup>(
-                File.ReadAllText(Path.Combine(DataLoader.TestSamplesDirectory, "5.json")));
+                File.ReadAllText(Path.Combine(TestSampleLoader.TestSamplesDirectory, "5.json")));
             var pipe2 = JsonConvert.DeserializeObject<RvmFacetGroup>(
-                File.ReadAllText(Path.Combine(DataLoader.TestSamplesDirectory, "6.json")));
+                File.ReadAllText(Path.Combine(TestSampleLoader.TestSamplesDirectory, "6.json")));
             var facetGroupsEqual = RvmFacetGroupMatcher.Match(pipe1, pipe2, out var transform);
             Assert.That(facetGroupsEqual);
         }
