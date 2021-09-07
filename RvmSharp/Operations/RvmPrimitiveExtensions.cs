643cdeca7c7f6ec21062a99b0c1f316935b45f65
Georgy Ushakov <gush@equinor.com>
2021-09-07 15:31:35 +0200

    Moved FacetGroup transform method to rvmsharp

---
+++ RvmSharp/Operations/RvmPrimitiveExtensions.cs
@@ -0,0 +1,32 @@
+﻿namespace RvmSharp.Operations
+{
+    using Primitives;
+    using System;
+    using System.Linq;
+    using System.Numerics;
+
+    public static class RvmPrimitiveExtensions
+    {
+        public static RvmFacetGroup TransformVertexData(this RvmFacetGroup group, Matrix4x4 matrix)
+        {
+            if (!Matrix4x4.Invert(matrix, out var matrixInverted))
+            {
+                throw new ArgumentException("Matrix cannot be inverted, to adjust normals we need to invert input matrix");
+            }
+            var matrixInvertedTransposed = Matrix4x4.Transpose(matrixInverted);
+            return group with
+            {
+                Polygons = group.Polygons.Select(a => a with
+                    {
+                        Contours = a.Contours.Select(c => c with
+                        {
+                            Vertices = c.Vertices.Select(v => (
+                                Vector3.Transform(v.Vertex, matrix),
+                                Vector3.Normalize(Vector3.TransformNormal(v.Normal, matrixInvertedTransposed)))).ToArray()
+                        }).ToArray()
+                    }
+                ).ToArray()
+            };
+        }
+    }
+}
