e8de5d55cf364a5f0673ce087a620606aa68363c
Espen Røvik Larsen <espla@equinor.com>
2021-10-05 13:13:35 +0200

    code cleanup

--- CadRevealComposer/Writers/F3dWriter.cs
+++ CadRevealComposer/Writers/F3dWriter.cs
@@ -8,13 +8,15 @@ namespace CadRevealComposer.Writers
 
     public static class F3dWriter
     {
-        private const uint MAGIC_BYTES = 0x4644_3346;
+        private const uint MagicBytes = 0x4644_3346;
+        private const uint FormatVersion = 8;
+        private const uint OptimizerVersion = 1;
 
         public record Scene(ImmutableArray<Sector> Sectors, int RootSectorId);
 
-        public record Sector(uint MagicBytes, uint FormatVersion, uint OptimizerVersion, ulong SectorId, ulong? ParentSectorId, Vector3 BboxMin, Vector3 BboxMax, SectorContents? SectorContents);
+        public record Sector(ulong SectorId, ulong? ParentSectorId, Vector3 BboxMin, Vector3 BboxMax, SectorContents? SectorContents);
 
-        public record SectorContents(ImmutableArray<uint> GridSize, Vector3 Origin, float GridIncrement, ImmutableArray<Node> Nodes);
+        public record SectorContents(uint GridSizeX, uint GridSizeY, uint GridSizeZ, Vector3 Origin, float GridIncrement, ImmutableArray<Node> Nodes);
 
         public record Node(CompressFlags CompressType, ulong NodeId, ulong TreeIndex, Color Color, ImmutableArray<Face> Faces);
 
@@ -23,31 +25,30 @@ namespace CadRevealComposer.Writers
         [Flags]
         public enum FaceFlags : byte
         {
-            POSITIVE_X_VISIBLE = 0b0000_0001,
-            POSITIVE_Y_VISIBLE = 0b0000_0010,
-            POSITIVE_Z_VISIBLE = 0b0000_0100,
-            NEGATIVE_X_VISIBLE = 0b0000_1000,
-            NEGATIVE_Y_VISIBLE = 0b0001_0000,
-            NEGATIVE_Z_VISIBLE = 0b0010_0000,
+            PositiveXVisible = 0b0000_0001,
+            PositiveYVisible = 0b0000_0010,
+            PositiveZVisible = 0b0000_0100,
+            NegativeXVisible = 0b0000_1000,
+            NegativeYVisible = 0b0001_0000,
+            NegativeZVisible = 0b0010_0000,
             // 0b0100_0000 is reserved and left out to trigger an error if used
-            MULTIPLE = 0b1000_0000
+            Multiple = 0b1000_0000
         }
 
         [Flags]
         public enum CompressFlags : byte
         {
-            POSITIVE_X_REPEAT_Z = 0b0000_0001,
-            POSITIVE_Y_REPEAT_Z = 0b0000_0010,
-            POSITIVE_Z_REPEAT_Y = 0b0000_0100,
-            NEGATIVE_X_REPEAT_Z = 0b0000_1000,
-            NEGATIVE_Y_REPEAT_Z = 0b0001_0000,
-            NEGATIVE_Z_REPEAT_Y = 0b0010_0000,
-            HAS_COLOR_ON_EACH_CELL = 0b0100_0000,
-            INDEX_IS_LONG = 0b1000_0000
+            PositiveXRepeatZ = 0b0000_0001,
+            PositiveYRepeatZ = 0b0000_0010,
+            PositiveZRepeatY = 0b0000_0100,
+            NegativeXRepeatZ = 0b0000_1000,
+            NegativeYRepeatZ = 0b0001_0000,
+            NegativeZRepeatY = 0b0010_0000,
+            HasColorOnEachCell = 0b0100_0000,
+            IndexIsLong = 0b1000_0000
         }
 
-
-        public static void WriteSector(Scene scene, Stream stream)
+        public static void WriteScene(Scene scene, Stream stream)
         {
             foreach (var sector in scene.Sectors)
             {
@@ -57,9 +58,9 @@ namespace CadRevealComposer.Writers
 
         private static void WriteSector(Sector sector, Stream stream)
         {
-            stream.WriteUint32(MAGIC_BYTES);
-            stream.WriteUint32(sector.FormatVersion);
-            stream.WriteUint32(sector.OptimizerVersion);
+            stream.WriteUint32(MagicBytes);
+            stream.WriteUint32(FormatVersion);
+            stream.WriteUint32(OptimizerVersion);
             stream.WriteUint64(sector.SectorId);
             stream.WriteUint64(sector.ParentSectorId ?? ulong.MaxValue);
 
@@ -77,9 +78,9 @@ namespace CadRevealComposer.Writers
             else
             {
                 stream.WriteUint32((uint)sector.SectorContents.Nodes.Length);
-                stream.WriteUint32(sector.SectorContents.GridSize[0]);
-                stream.WriteUint32(sector.SectorContents.GridSize[1]);
-                stream.WriteUint32(sector.SectorContents.GridSize[2]);
+                stream.WriteUint32(sector.SectorContents.GridSizeX);
+                stream.WriteUint32(sector.SectorContents.GridSizeY);
+                stream.WriteUint32(sector.SectorContents.GridSizeZ);
                 stream.WriteFloat(sector.SectorContents.Origin.X);
                 stream.WriteFloat(sector.SectorContents.Origin.Y);
                 stream.WriteFloat(sector.SectorContents.Origin.Z);
@@ -94,8 +95,8 @@ namespace CadRevealComposer.Writers
 
         private static void WriteNode(Node node, Stream stream)
         {
-            var hasColorOnEachCell = (node.CompressType & CompressFlags.HAS_COLOR_ON_EACH_CELL) == CompressFlags.HAS_COLOR_ON_EACH_CELL;
-            var indexAsUInt64 = (node.CompressType & CompressFlags.INDEX_IS_LONG) == CompressFlags.INDEX_IS_LONG;
+            var hasColorOnEachCell = node.CompressType.HasFlag(CompressFlags.HasColorOnEachCell);
+            var indexAsUInt64 = node.CompressType.HasFlag(CompressFlags.IndexIsLong);
 
             stream.WriteUint64(node.NodeId);
             stream.WriteUint64(node.TreeIndex);
@@ -104,10 +105,6 @@ namespace CadRevealComposer.Writers
 
             if (!hasColorOnEachCell)
             {
-                // TODO: verify component order
-                // TODO: verify component order
-                // TODO: verify component order
-
                 stream.WriteByte(node.Color.R);
                 stream.WriteByte(node.Color.G);
                 stream.WriteByte(node.Color.B);
@@ -121,7 +118,7 @@ namespace CadRevealComposer.Writers
 
         private static void WriteFace(Face face, Stream stream, bool hasColorOnEachCell, bool indexAsUInt64)
         {
-            var multiple = (face.FaceFlags & FaceFlags.MULTIPLE) == FaceFlags.MULTIPLE;
+            var multiple = face.FaceFlags.HasFlag(FaceFlags.Multiple);
 
             if (indexAsUInt64)
             {
@@ -135,10 +132,6 @@ namespace CadRevealComposer.Writers
             stream.WriteByte(multiple ? face.Repetitions : (byte)0);
             if (hasColorOnEachCell)
             {
-                // TODO: verify component order
-                // TODO: verify component order
-                // TODO: verify component order
-
                 stream.WriteByte(face.Color.R);
                 stream.WriteByte(face.Color.G);
                 stream.WriteByte(face.Color.B);
