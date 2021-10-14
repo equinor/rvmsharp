0748ced892ef134c946d9bef5646dcabc0076c94
Nils Henrik Hals <nih@equinor.com>
2021-10-14 09:15:34 +0200

    Add xml-docs

--- RvmSharp/Primitives/RvmPyramid.cs
+++ RvmSharp/Primitives/RvmPyramid.cs
@@ -2,7 +2,19 @@
 {
     using System.Numerics;
 
-
+    /// <summary>
+    /// RvmPyramid Primitive
+    /// </summary>
+    /// <param name="Version"></param>
+    /// <param name="Matrix"></param>
+    /// <param name="BoundingBoxLocal"></param>
+    /// <param name="BottomX">The width at bottom X</param>
+    /// <param name="BottomY">The depth at bottom Y</param>
+    /// <param name="TopX">The width at the top X</param>
+    /// <param name="TopY">The depth at the top Y</param>
+    /// <param name="OffsetX">The offset from center bottom to center top X</param>
+    /// <param name="OffsetY">The offset from center bottom to center top Y</param>
+    /// <param name="Height">The Height difference between bottom and top plane</param>
     public record RvmPyramid(
             uint Version,
             Matrix4x4 Matrix,
