155545497410ec0bf8663255c9e864eca8e09821
Nils Henrik Hals <nih@equinor.com>
2021-10-14 09:15:20 +0200

    Make NodeIdProvider more thread safe

--- CadRevealComposer/IdProviders/NodeIdProvider.cs
+++ CadRevealComposer/IdProviders/NodeIdProvider.cs
@@ -1,14 +1,15 @@
 ﻿namespace CadRevealComposer.IdProviders
 {
     using System;
-    using System.Collections.Generic;
+    using System.Collections.Concurrent;
 
     // FIXME: placeholder implementation
     public class NodeIdProvider
     {
         private readonly Random _random = new();
 
-        private readonly HashSet<ulong> _generatedIds = new();
+        // Using ConcurrentDict as a substitute for the non-existent ConcurrentHashSet.
+        private readonly ConcurrentDictionary<ulong, byte> _generatedIds = new();
 
         // TODO: this will generate or fetch Node ID based on project, hierarchy, name. The idea is to keep it deterministic if possible
         public ulong GetNodeId(CadRevealNode? cadNode)
@@ -17,9 +18,8 @@
             do
             {
                 value = (uint)_random.Next(0, Int32.MaxValue); // TODO: Expand to Javascript Safe Number range ((2^53)-1)
-            } while (_generatedIds.Contains(value));
+            } while (!_generatedIds.TryAdd(value, 0));
 
-            _generatedIds.Add(value);
             return value;
         }
     }
