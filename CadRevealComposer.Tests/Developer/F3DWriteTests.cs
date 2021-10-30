a260bf45916ed8841255ddb301097082181d4926
Georgy Ushakov <gush@equinor.com>
2021-10-31 01:56:09 +0200

    Working on faces conversion

---
+++ CadRevealComposer.Tests/Developer/F3DWriteTests.cs
@@ -0,0 +1,32 @@
+﻿namespace CadRevealComposer.Tests.Developer
+{
+    using Faces;
+    using NUnit.Framework;
+    using System.Drawing;
+    using System.IO;
+    using System.Numerics;
+    using Writers;
+
+    [TestFixture]
+    public class F3DWriteTests
+    {
+        [Test]
+        public void SimpleCube()
+        {
+            var f3d = new SectorFaces(0, null, Vector3.Zero, Vector3.One * 30,
+                new FacesGrid(new GridParameters(2, 2, 2, Vector3.Zero, 5f),
+                    new []
+                    {
+                        new Node(CompressFlags.HasColorOnEachCell, 1, 1, null,
+                            new []
+                            {
+                                new Face(FaceFlags.PositiveXVisible | FaceFlags.PositiveYVisible,
+                                    0, 0, Color.Blue)
+                            })
+                    }));
+            using var outputStream = File.Create(@"E:\gush\projects\echo\echo-web\reveal-master\examples\public\primitives\sector_0.f3d");
+            F3dWriter.WriteSector(f3d, outputStream);
+        }
+
+    }
+}
---
+++ CadRevealComposer/Faces/Node.cs
@@ -0,0 +1,43 @@
+﻿namespace CadRevealComposer.Faces
+{
+    using System;
+    using System.Drawing;
+
+    [Flags]
+    public enum CompressFlags : byte
+    {
+        PositiveXRepeatZ = 0b0000_0001,
+        PositiveYRepeatZ = 0b0000_0010,
+        PositiveZRepeatY = 0b0000_0100,
+        NegativeXRepeatZ = 0b0000_1000,
+        NegativeYRepeatZ = 0b0001_0000,
+        NegativeZRepeatY = 0b0010_0000,
+        HasColorOnEachCell = 0b0100_0000,
+        IndexIsLong = 0b1000_0000
+    }
+
+    public record Node
+    {
+        public Node(CompressFlags compressFlags, ulong nodeId, ulong treeIndex, Color? color, Face[] faces)
+        {
+            CompressFlags = compressFlags;
+            NodeId = nodeId;
+            TreeIndex = treeIndex;
+            Color = color;
+            Faces = faces;
+        }
+
+        public CompressFlags CompressFlags { get; }
+
+        public ulong NodeId { get; }
+
+        public ulong TreeIndex { get; }
+
+        /// <summary>
+        /// Optional color, must be set if CompressFlags.HasColorOnEachCell is NOT set
+        /// </summary>
+        public Color? Color { get; }
+
+        public Face[] Faces { get; }
+    }
+}
