4882cd827cebbf05900246fef660b52c4d0197a8
Nils Henrik Hals <nih@equinor.com>
2021-11-02 11:24:15 +0100

    Remove dead Pyramid code and improve tests

--- CadRevealComposer.Tests/Operations/RvmPyramidInstancerTests.cs
+++ CadRevealComposer.Tests/Operations/RvmPyramidInstancerTests.cs
@@ -5,29 +5,90 @@
     using NUnit.Framework;
     using RvmSharp.Primitives;
     using System.Drawing;
+    using System.Linq;
     using System.Numerics;
 
     [TestFixture]
     public class RvmPyramidInstancerTests
     {
+        private RvmBoundingBox _throwawayBoundingBox => new RvmBoundingBox(Vector3.Zero, Vector3.Zero);
+
         [Test]
         public void Process_WhenTwoIdenticalMeshes_IgnoresOneOfThem()
         {
             // Arbitrary arguments.
-            var rvmPyramid = new RvmPyramid(2, Matrix4x4.Identity, new RvmBoundingBox(Vector3.One, Vector3.One), 1, 1,
+            var rvmPyramid = new RvmPyramid(2, Matrix4x4.Identity, _throwawayBoundingBox, 1, 1,
                 1, 1, 1, 1, 1);
+
+            // Arbitrary arguments.
+            var rvmPyramidNotMAtching = new RvmPyramid(2, Matrix4x4.Identity, _throwawayBoundingBox, 1, 1,
+                1, 1, 2, 2, 1);
 
             var props = new CommonPrimitiveProperties(
                 1, 1, Vector3.Zero, Quaternion.Identity, Vector3.One, 0, new RvmBoundingBox(Vector3.One, Vector3.One),
                 Color.Aqua, (Vector3.One, 0));
 
             // Mark: These two input pyramids will be identical as they are Records with identical values.
-            ProtoMeshFromPyramid[] protoPyramids = new []{new ProtoMeshFromPyramid(props, rvmPyramid), new ProtoMeshFromPyramid(props, rvmPyramid)};
+            ProtoMeshFromPyramid[] protoPyramids = new[]
+            {
+                new ProtoMeshFromPyramid(props, rvmPyramid),
+                new ProtoMeshFromPyramid(props, rvmPyramid),
+                new ProtoMeshFromPyramid(props, rvmPyramidNotMAtching)
+            };
 
             var res =
                 RvmPyramidInstancer.Process(protoPyramids);
 
-            Assert.That(res, Has.One.Items);
+            Assert.That(res, Has.Exactly(2).Items);
+        }
+
+        [Test]
+        public void TwoPyramidsWithSimilarProportionsAreTheSame()
+        {
+            var rvmPyramidA = new RvmPyramid(Version: 2,
+                Matrix: Matrix4x4.Identity,
+                BoundingBoxLocal: _throwawayBoundingBox,
+                BottomX: 2,
+                BottomY: 4,
+                TopX: 6,
+                TopY: 1,
+                OffsetX: 2,
+                OffsetY: 3,
+                Height: 1);
+
+
+            var rvmPyramidAHalfScaled = new RvmPyramid(Version: 2,
+                Matrix: Matrix4x4.Identity,
+                BoundingBoxLocal: _throwawayBoundingBox,
+                BottomX: 1,
+                2,
+                3,
+                0.5f,
+                1,
+                1.5f,
+                2f);
+
+            var rvmPyramidCUnique =
+                rvmPyramidA with
+                {
+                    TopX = rvmPyramidA.TopX + 1
+                }; // Change proportions of a dimension (Should not match)
+            var arbitraryProps = new CommonPrimitiveProperties(0, 1, Vector3.Zero, Quaternion.Identity, Vector3.One,
+                1,
+                _throwawayBoundingBox, Color.Aqua, (Vector3.One, 0));
+            var protoPyramids = new[] { rvmPyramidA, rvmPyramidAHalfScaled, rvmPyramidCUnique }.Select(rvmPyramid =>
+                new ProtoMeshFromPyramid(arbitraryProps, rvmPyramid)).ToArray();
+
+
+            Assert.That(rvmPyramidA, Is.Not.EqualTo(rvmPyramidAHalfScaled));
+
+            var templateLookup = RvmPyramidInstancer.Process(protoPyramids);
+
+            var templates = templateLookup.Values.Select(x => x.template).ToArray();
+
+            Assert.That(templates, Contains.Item(rvmPyramidA));
+            Assert.That(templates, Contains.Item(rvmPyramidCUnique));
+            Assert.That(templates, Does.Not.Contain(rvmPyramidAHalfScaled)); // This should have been templated
         }
     }
 }
