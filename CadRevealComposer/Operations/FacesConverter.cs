aa6195b8667e27509adeca03d597ad8e4ef958bc
Georgy Ushakov <gush@equinor.com>
2021-11-15 15:27:51 +0100

    Raycast code

--- CadRevealComposer/Operations/FacesConverter.cs
+++ CadRevealComposer/Operations/FacesConverter.cs
@@ -7,11 +7,25 @@
     public static class FacesConverter
     {
 
+        public record ProtoGrid();
+
 
         public static ProtoGrid Convert(Mesh inputMesh, GridParameters gridParameters)
         {
             var triangleCount = inputMesh.Triangles.Count / 3;
-            for (var i = triangleCount; i < triangleCount; i++)
+            //for (var i = triangleCount; i < triangleCount; i++)
+
+            // Pseudo code
+            // - for each triangle
+            // -    get bounds
+            // -    define squares to raycast from
+            // -    ray-casting
+            // -    write results into protogrid
+
+
+            // Separate protogrid per triangle, merged at the end
+            //
+            return new ProtoGrid();
         }
 
     }
