05311741178846bfa06fa51f1e678538f81b92c3
Espen Røvik Larsen <espenrl@users.noreply.github.com>
2022-01-19 13:24:01 +0100

    format and optimize faces code (#58)

--- CadRevealComposer/AlgebraExtensions/Ray.cs
+++ CadRevealComposer/AlgebraExtensions/Ray.cs
@@ -4,21 +4,24 @@ namespace CadRevealComposer.AlgebraExtensions
     using System.Numerics;
     using Utils;
 
-    public record Ray(Vector3 Origin, Vector3 Direction)
+    public readonly record struct Ray(Vector3 Origin, Vector3 Direction)
     {
-        public bool Trace(Triangle triangle, out Vector3 intersectionPoint, out bool isFrontFace)
+        public bool Trace(in Triangle triangle, out Vector3 intersectionPoint, out bool isFrontFace)
         {
-            const float tolerance = 0.00001f;
+            const float tolerance = 0.000_01f;
 
             intersectionPoint = Vector3.Zero;
             isFrontFace = false;
+
             var v1v2 = triangle.V2 - triangle.V1;
             var v1v3 = triangle.V3 - triangle.V1;
             var v2v3 = triangle.V3 - triangle.V2;
             var v3v1 = triangle.V1 - triangle.V3;
             var planeNormal = Vector3.Cross(v1v2, v1v3);
-            if (planeNormal.LengthSquared() < 0.0000000001f) // NOTE: arbitrary value, works fine for now
+            if (planeNormal.LengthSquared() < 0.000_000_0001f) // NOTE: arbitrary value, works fine for now
+            {
                 return false; // Triangle is too small for raycast
+            }
             planeNormal = Vector3.Normalize(planeNormal);
 
             // Plane formula
@@ -27,13 +30,16 @@ namespace CadRevealComposer.AlgebraExtensions
             // n * [x y z]T = d
             // Ray(t) = Ro + t * Rd
             // t = (d - n * Ro)/(n * Rd)
-            var d = Vector3.Dot(planeNormal, triangle.V1);
             if (Vector3.Dot(planeNormal, Direction).ApproximatelyEquals(0))
+            {
                 return false;
-            var t =
-                (d - Vector3.Dot(planeNormal, Origin)) / Vector3.Dot(planeNormal, Direction);
+            }
+
+            var t = (Vector3.Dot(planeNormal, triangle.V1) - Vector3.Dot(planeNormal, Origin)) / Vector3.Dot(planeNormal, Direction);
             if (t < 0)
+            {
                 return false;
+            }
 
             intersectionPoint = Origin + Direction * t;
             isFrontFace = planeNormal.AngleTo(Direction) > Math.PI / 2;
@@ -42,13 +48,15 @@ namespace CadRevealComposer.AlgebraExtensions
             var v1pi = intersectionPoint - triangle.V1;
             var v2pi = intersectionPoint - triangle.V2;
             var v3pi = intersectionPoint - triangle.V3;
+
             // Check if intersection point is on any corner
             if (v1pi.EqualsWithinTolerance(Vector3.Zero, tolerance) ||
                 v2pi.EqualsWithinTolerance(Vector3.Zero, tolerance) ||
                 v3pi.EqualsWithinTolerance(Vector3.Zero, tolerance))
+            {
                 return true;
+            }
 
-            // Check if intersection point is on any side
             var v1v2Crossv1pi = Vector3.Cross(v1v2, v1pi);
             var v2v3Crossv2pi = Vector3.Cross(v2v3, v2pi);
             var v3v1Crossv3pi = Vector3.Cross(v3v1, v3pi);
@@ -58,24 +66,30 @@ namespace CadRevealComposer.AlgebraExtensions
             {
                 if ((triangle.V2 - intersectionPoint).AngleTo(v1v2).ApproximatelyEquals(0) &&
                     v1pi.AngleTo(v1v2).ApproximatelyEquals(0))
+                {
                     return true;
+                }
             }
             if (v2v3Crossv2pi.EqualsWithinTolerance(Vector3.Zero, tolerance))
             {
                 if ((triangle.V3 - intersectionPoint).AngleTo(v2v3).ApproximatelyEquals(0) &&
                     v2pi.AngleTo(v2v3).ApproximatelyEquals(0))
+                {
                     return true;
+                }
             }
             if (v3v1Crossv3pi.EqualsWithinTolerance(Vector3.Zero, tolerance))
             {
                 if ((triangle.V1 - intersectionPoint).AngleTo(v3v1).ApproximatelyEquals(0) &&
                     v3pi.AngleTo(v3v1).ApproximatelyEquals(0))
+                {
                     return true;
+                }
             }
 
-            var aboveV1V2 = (Vector3.Dot(Vector3.Normalize(v1v2Crossv1pi), planeNormal) >= 0.0);
-            var aboveV2V3 = (Vector3.Dot(Vector3.Normalize(v2v3Crossv2pi), planeNormal) >= 0.0);
-            var aboveV3V1 = (Vector3.Dot(Vector3.Normalize(v3v1Crossv3pi), planeNormal) >= 0.0);
+            var aboveV1V2 = Vector3.Dot(Vector3.Normalize(v1v2Crossv1pi), planeNormal) >= 0.0;
+            var aboveV2V3 = Vector3.Dot(Vector3.Normalize(v2v3Crossv2pi), planeNormal) >= 0.0;
+            var aboveV3V1 = Vector3.Dot(Vector3.Normalize(v3v1Crossv3pi), planeNormal) >= 0.0;
 
             return aboveV1V2 && aboveV2V3 && aboveV3V1;
         }
--- CadRevealComposer/AlgebraExtensions/Vector3i.cs
+++ CadRevealComposer/AlgebraExtensions/Vector3i.cs
@@ -1,23 +1,11 @@
 namespace CadRevealComposer.AlgebraExtensions
 {
-    using System;
     using System.Numerics;
 
     // ReSharper disable once InconsistentNaming
-    public struct Vector3i : IEquatable<Vector3i>
+    public readonly record struct Vector3i(int X, int Y, int Z)
     {
-        public static Vector3i One => new(1, 1, 1);
-
-        public int X;
-        public int Y;
-        public int Z;
-
-        public Vector3i(int x, int y, int z)
-        {
-            X = x;
-            Y = y;
-            Z = z;
-        }
+        public static Vector3i One { get; } = new(1, 1, 1);
 
         public static Vector3 operator *(Vector3i a, float b)
         {
@@ -27,66 +15,6 @@ namespace CadRevealComposer.AlgebraExtensions
         public static Vector3i operator +(Vector3i a, Vector3i b)
         {
             return new Vector3i(a.X + b.X, a.Y + b.Y, a.Z + b.Z);
-        }
-
-        public int this[int i]
-        {
-            get
-            {
-                return i switch
-                {
-                    0 => X,
-                    1 => Y,
-                    2 => Z,
-                    _ => throw new IndexOutOfRangeException()
-                };
-            }
-            set
-            {
-                switch (i)
-                {
-                    case 0:
-                        X = value;
-                        break;
-                    case 1:
-                        Y = value;
-                        break;
-                    case 2:
-                        Z = value;
-                        break;
-                    default:
-                        throw new IndexOutOfRangeException();
-                }
-            }
-        }
-
-        public override bool Equals(object? obj)
-        {
-            return obj switch
-            {
-                Vector3i a => Equals(a),
-                _ => false
-            };
-        }
-
-        public override int GetHashCode()
-        {
-            return HashCode.Combine(X, Y, Z);
-        }
-
-        public bool Equals(Vector3i other)
-        {
-            return X == other.X && Y == other.Y && Z == other.Z;
-        }
-
-        public static bool operator ==(Vector3i left, Vector3i right)
-        {
-            return left.Equals(right);
-        }
-
-        public static bool operator !=(Vector3i left, Vector3i right)
-        {
-            return !(left == right);
         }
     }
 }
