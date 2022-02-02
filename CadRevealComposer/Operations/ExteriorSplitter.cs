0a1109de6d4bb903d35c5da1f48432b872fa41c7
Espen Røvik Larsen <espla@equinor.com>
2022-02-02 20:48:28 +0100

    exterior splitting

---
+++ CadRevealComposer/Operations/ExteriorSplitter.cs
@@ -0,0 +1,275 @@
+namespace CadRevealComposer.Operations;
+
+using AlgebraExtensions;
+using Primitives;
+using RvmSharp.Primitives;
+using RvmSharp.Tessellation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Runtime.CompilerServices;
+using Utils;
+
+/// <summary>
+/// Split geometries into exterior and interior.
+/// This is to aid sector splitting, prioritizing the exterior higher up in the sector tree.
+/// </summary>
+public static class ExteriorSplitter
+{
+    private record Primitive(APrimitive OriginalPrimitive);
+    private sealed record TesselatedPrimitive(Triangle[] Triangles, APrimitive OriginalPrimitive) : Primitive(OriginalPrimitive);
+
+    private readonly record struct Node(Bounds BoundingBox, Primitive[] Primitives);
+
+    private readonly record struct RayEx(Bounds Box, Ray Ray);
+
+    private static IEnumerable<RayEx> CreateRays(Vector3 boundingBoxMin, Vector3 boundingBoxMax)
+    {
+        const float cellSize = 1f;
+        const float halfCell = cellSize / 2f;
+
+        // calculate grid size
+        var gridMin = new Vector3(
+            MathF.Floor(boundingBoxMin.X),
+            MathF.Floor(boundingBoxMin.Y),
+            MathF.Floor(boundingBoxMin.Z)
+        );
+
+        var gridMax = new Vector3(
+            MathF.Ceiling(boundingBoxMax.X),
+            MathF.Ceiling(boundingBoxMax.Y),
+            MathF.Ceiling(boundingBoxMax.Z)
+        );
+
+        var rayOriginMin = gridMin - new Vector3(100f);
+        var rayOriginMax = gridMax + new Vector3(100f);
+
+        // rays for XY plane
+        for (float x = gridMin.X; x < gridMax.X; x += cellSize)
+            for (float y = gridMin.Y; y < gridMax.Y; y += cellSize)
+            {
+                var bounds = new Bounds(
+                    new Vector3(x, y, rayOriginMin.Z),
+                    new Vector3(x + cellSize, y + cellSize, rayOriginMax.Z)
+                    );
+
+                // negative
+                yield return new RayEx(
+                    bounds,
+                    new Ray(new Vector3(x + halfCell, y + halfCell, rayOriginMin.Z), Vector3.UnitZ));
+
+                // positive
+                yield return new RayEx(
+                    bounds,
+                    new Ray(new Vector3(x + halfCell, y + halfCell, rayOriginMax.Z), -Vector3.UnitZ));
+            }
+
+        // rays for XZ plane
+        for (float x = gridMin.X; x < gridMax.X; x += cellSize)
+            for (float z = gridMin.Z; z < gridMax.Z; z += cellSize)
+            {
+                var bounds = new Bounds(
+                    new Vector3(x, rayOriginMin.Y, z),
+                    new Vector3(x + cellSize, rayOriginMax.Y, z + cellSize)
+                );
+
+                // negative
+                yield return new RayEx(
+                    bounds,
+                    new Ray(new Vector3(x + halfCell, rayOriginMin.Y, z + halfCell), Vector3.UnitY));
+
+                // positive
+                yield return new RayEx(
+                    bounds,
+                    new Ray(new Vector3(x + halfCell, rayOriginMax.Y, z + halfCell), -Vector3.UnitY));
+            }
+
+        // rays for YZ plane
+        for (float y = gridMin.Y; y < gridMax.Y; y += cellSize)
+            for (float z = gridMin.Z; z < gridMax.Z; z += cellSize)
+            {
+                var bounds = new Bounds(
+                    new Vector3(rayOriginMin.X, y, z),
+                    new Vector3(rayOriginMax.X, y + cellSize, z + cellSize)
+                );
+
+                // negative
+                yield return new RayEx(
+                    bounds,
+                    new Ray(new Vector3(rayOriginMin.X, y + halfCell, z + halfCell), Vector3.UnitX));
+
+                // positive
+                yield return new RayEx(
+                    bounds,
+                    new Ray(new Vector3(rayOriginMax.X, y + halfCell, z + halfCell), -Vector3.UnitX));
+            }
+    }
+
+    public static (APrimitive[] Exterior, APrimitive[] Interior) Split(APrimitive[] primitives)
+    {
+        var nodes = CreateNodes(primitives);
+
+        // create k-d tree for efficient processing of nodes
+        var bbMin = primitives.GetBoundingBoxMin();
+        var bbMax = primitives.GetBoundingBoxMax();
+        var tree = new IntervalKdTree<Node>(bbMin, bbMax, 100);
+        foreach (var node in nodes)
+        {
+            tree.Put(node.BoundingBox.Min, node.BoundingBox.Max, node);
+        }
+
+        // ray casting
+        IEnumerable<Node> TraceRay(RayEx ray)
+        {
+            var distance = float.MaxValue;
+            Node? node = null;
+
+            var potentialNodes = tree.GetValues(ray.Box.Min, ray.Box.Max);
+            foreach (var potentialNode in potentialNodes)
+            {
+                foreach (var primitive in potentialNode.Primitives)
+                {
+                    var result = primitive switch
+                    {
+                        TesselatedPrimitive rayCast => MatchRayCast(rayCast.Triangles, ray.Ray),
+                        _ => MatchBoundingBox(primitive.OriginalPrimitive.AxisAlignedBoundingBox, ray.Box, ray.Ray)
+                    };
+                    if (result.Hit && result.Distance < distance)
+                    {
+                        distance = result.Distance;
+                        node = potentialNode;
+                    }
+                }
+            }
+
+            if (node.HasValue)
+            {
+                yield return node.Value;
+            }
+        }
+
+        var exteriorNodeSet = CreateRays(bbMin, bbMax)
+            .AsParallel()
+            .SelectMany(TraceRay)
+            .ToHashSet();
+
+        var exterior = exteriorNodeSet
+            .SelectMany(n => n.Primitives.Select(p => p.OriginalPrimitive))
+            .ToArray();
+        var interior = primitives
+            .Except(exterior)
+            .ToArray();
+
+        return (exterior, interior);
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining | MethodImplOptions.AggressiveOptimization)]
+    private static (bool Hit, float Distance) MatchRayCast(Triangle[] triangles, Ray ray)
+    {
+        foreach (var triangle in triangles)
+        {
+            if (ray.Trace(triangle, out var intersectionPoint, out var isFrontFace))
+            {
+                var distance = (ray.Origin - intersectionPoint).Length();
+                return (true, distance);
+            }
+        }
+
+        return (false, float.NaN);
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining | MethodImplOptions.AggressiveOptimization)]
+    private static (bool Hit, float Distance) MatchBoundingBox(RvmBoundingBox boundingBox, Bounds rayBounds, Ray ray)
+    {
+        // positive if overlaps
+        var diff = Vector3.Min(boundingBox.Max, rayBounds.Max) - Vector3.Max(boundingBox.Min, rayBounds.Min);
+        var isHit = diff.X > 0f &&
+                    diff.Y > 0f &&
+                    diff.Z > 0f;
+        if (isHit)
+        {
+            float distance;
+            if (ray.Direction.X < 0f)
+            {
+                distance = boundingBox.Min.X - ray.Origin.X;
+            }
+            else if (ray.Direction.Y < 0f)
+            {
+                distance = boundingBox.Min.Y - ray.Origin.Y;
+            }
+            else if (ray.Direction.Z < 0f)
+            {
+                distance = boundingBox.Min.Z - ray.Origin.Z;
+            }
+            else if (ray.Direction.X > 0f)
+            {
+                distance = ray.Origin.X - boundingBox.Max.X;
+            }
+            else if (ray.Direction.Y > 0f)
+            {
+                distance = ray.Origin.Y - boundingBox.Max.Y;
+            }
+            else
+            {
+                distance = ray.Origin.Z - boundingBox.Max.Z;
+            }
+            return (true, distance);
+        }
+
+        return (false, float.NaN);
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private static IEnumerable<Triangle> CollectTrianglesForMesh(Mesh mesh)
+    {
+        var triangleCount = mesh.Triangles.Count / 3;
+        var vertices = mesh.Vertices;
+        for (var i = 0; i < triangleCount; i++)
+        {
+            var v1 = vertices[mesh.Triangles[i * 3]];
+            var v2 = vertices[mesh.Triangles[i * 3 + 1]];
+            var v3 = vertices[mesh.Triangles[i * 3 + 2]];
+            yield return new Triangle(v1, v2, v3);
+        }
+    }
+
+    private static Node[] CreateNodes(APrimitive[] primitives)
+    {
+        static TesselatedPrimitive? TesselatePrimitive(APrimitive primitive)
+        {
+            var mesh = TessellatorBridge.Tessellate(primitive.SourcePrimitive, 1.0f);
+            if (mesh is null)
+            {
+                return null;
+            }
+            var triangles = CollectTrianglesForMesh(mesh).ToArray();
+            return new TesselatedPrimitive(triangles, primitive);
+        }
+
+        static Node ConvertNode(IGrouping<ulong, APrimitive> nodeGroup)
+        {
+            var bbMin = nodeGroup.GetBoundingBoxMin();
+            var bbMax = nodeGroup.GetBoundingBoxMax();
+            var boundingBox = new Bounds(bbMin, bbMax);
+
+            var primitives = nodeGroup
+                .Select(p => p switch
+                {
+                    InstancedMesh instancedMesh => TesselatePrimitive(instancedMesh),
+                    TriangleMesh triangleMesh => TesselatePrimitive(triangleMesh),
+                    _ => new Primitive(p)
+                })
+                .WhereNotNull()
+                .ToArray();
+
+            return new Node(boundingBox, primitives);
+        }
+
+        return primitives
+            .GroupBy(p => p.NodeId)
+            .AsParallel()
+            .Select(ConvertNode)
+            .ToArray();
+    }
+}
---
+++ CadRevealComposer/Operations/IntervalKDTree.cs
@@ -0,0 +1,209 @@
+namespace CadRevealComposer.Operations;
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Numerics;
+
+public class IntervalKdTree<T> where T : notnull
+{
+    private readonly Node _rootNode;
+    private readonly int _divisionThreshold;
+
+    public IntervalKdTree(Vector3 min, Vector3 max, int divisionThreshold)
+    {
+        _divisionThreshold = divisionThreshold;
+        _rootNode = new Node(
+            this,
+            0,
+            min,
+            max);
+    }
+
+    public void Put(Vector3 min, Vector3 max, T value)
+    {
+        _rootNode.AddBox(new Box(value, min, max));
+    }
+
+    public IEnumerable<T> GetValues(Vector3 min, Vector3 max)
+    {
+        return _rootNode.GetValues(new Cube(min, max));
+    }
+
+    /// <summary>
+    /// Node is extend axis aligned 3d cube which is the node implementation of IntervalKDTree.
+    /// </summary>
+    private record Node : Cube
+    {
+        private readonly int _depth;
+        private float _divisionBoundary;
+
+        [MemberNotNullWhen(true, nameof(_lowChild), nameof(_highChild))]
+        private bool HasChildren() => _lowChild is not null && _highChild is not null;
+
+        private Node? _lowChild;
+        private Node? _highChild;
+
+        private readonly IntervalKdTree<T> _tree;
+        private List<Box> _boxes;
+
+        public Node(
+            IntervalKdTree<T> tree,
+            int depth,
+            Vector3 min,
+            Vector3 max)
+            : base(min, max)
+        {
+            _tree = tree;
+            _depth = depth;
+            _boxes = new List<Box>();
+        }
+
+        public void AddBox(Box box)
+        {
+            if (HasChildren())
+            {
+                if (box.IsBelow(_depth, _divisionBoundary))
+                {
+                    _lowChild.AddBox(box);
+                    return;
+                }
+                if (box.IsAbove(_depth, _divisionBoundary))
+                {
+                    _highChild.AddBox(box);
+                    return;
+                }
+            }
+
+            _boxes.Add(box);
+
+            // Divide to children if threshold has been exceeded
+            if (!HasChildren() && _boxes.Count > _tree._divisionThreshold)
+            {
+                Divide();
+            }
+        }
+
+        public IEnumerable<T> GetValues(Cube cube)
+        {
+            foreach (var box in _boxes)
+            {
+                if (cube.Intersects(box))
+                {
+                    yield return box.Value;
+                }
+            }
+
+            if (HasChildren())
+            {
+                if (cube.IsBelow(_depth, _divisionBoundary))
+                {
+                    foreach (var value in _lowChild.GetValues(cube))
+                    {
+                        yield return value;
+                    }
+                }
+                else if (cube.IsAbove(_depth, _divisionBoundary))
+                {
+                    foreach (var value in _highChild.GetValues(cube))
+                    {
+                        yield return value;
+                    }
+                }
+                else
+                {
+                    foreach (var value in _lowChild.GetValues(cube))
+                    {
+                        yield return value;
+                    }
+                    foreach (var value in _highChild.GetValues(cube))
+                    {
+                        yield return value;
+                    }
+                }
+            }
+        }
+
+        private void Divide()
+        {
+            if (HasChildren())
+            {
+                throw new Exception("Already has children.");
+            }
+
+            int dimension = _depth % 3;
+
+            _divisionBoundary = dimension switch
+            {
+                0 => (Max.X + Min.X) / 2,
+                1 => (Max.Y + Min.Y) / 2,
+                _ => (Max.Z + Min.Z) / 2
+            };
+
+            _lowChild = dimension switch
+            {
+                0 => new Node(_tree, _depth + 1, Min, new Vector3(_divisionBoundary, Max.Y, Max.Z)),
+                1 => new Node(_tree, _depth + 1, Min, new Vector3(Max.X, _divisionBoundary, Max.Z)),
+                _ => new Node(_tree, _depth + 1, Min, new Vector3(Max.X, Max.Y, _divisionBoundary))
+            };
+
+            _highChild = dimension switch
+            {
+                0 => new Node(_tree, _depth + 1, new Vector3(_divisionBoundary, Min.Y, Min.Z), Max),
+                1 => new Node(_tree, _depth + 1, new Vector3(Min.X, _divisionBoundary, Min.Z), Max),
+                _ => new Node(_tree, _depth + 1, new Vector3(Min.X, Min.Y, _divisionBoundary), Max)
+            };
+
+            IList<Box> oldBoxList = _boxes;
+            _boxes = new List<Box>();
+            foreach (Box cube in oldBoxList)
+            {
+                AddBox(cube);
+            }
+
+        }
+    }
+
+    /// <summary>
+    /// Box is axis aligned 3d cube which can hold value. Acts as capsule in IntervalKDTree data structure.
+    /// </summary>
+    private record Box(T Value, Vector3 Min, Vector3 Max) : Cube(Min, Max);
+
+    /// <summary>
+    /// Axis aligned 3d cube implementation with math functions.
+    /// </summary>
+    private record Cube(Vector3 Min, Vector3 Max)
+    {
+        public bool IsBelow(int depth, double boundary)
+        {
+            int dimension = depth % 3;
+            return dimension switch
+            {
+                0 => Max.X < boundary,
+                1 => Max.Y < boundary,
+                _ => Max.Z < boundary
+            };
+        }
+
+        public bool IsAbove(int depth, double boundary)
+        {
+            int dimension = depth % 3;
+            return dimension switch
+            {
+                0 => boundary <= Min.X,
+                1 => boundary <= Min.Y,
+                _ => boundary <= Min.Z
+            };
+        }
+
+        public bool Intersects(Cube cube)
+        {
+            // positive if overlaps
+            var diff = Vector3.Min(Max, cube.Max) - Vector3.Max(Min, cube.Min);
+
+            return diff.X > 0f &&
+                   diff.Y > 0f &&
+                   diff.Z > 0f;
+        }
+    }
+}
