3d11da6031f2b1267fd1aba13e1537715b143596
Georgy Ushakov <gush@equinor.com>
2022-02-10 10:14:43 +0100

    Removed fixme and added additional obj export test

--- RvmSharp.Tests/Operations/RvmObjExporterTests.cs
+++ RvmSharp.Tests/Operations/RvmObjExporterTests.cs
@@ -1,6 +1,7 @@
 ﻿namespace RvmSharp.Tests.Operations;
 
 using Containers;
+using Exporters;
 using NUnit.Framework;
 using RvmSharp.Operations;
 using RvmSharp.Primitives;
@@ -8,6 +9,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Numerics;
+using Tessellation;
 
 [TestFixture]
 public class RvmObjExporterTests
@@ -38,4 +40,30 @@ public class RvmObjExporterTests
         Assert.That(materialRefCount.Equals(1), "OBJ Exporter should not introduce unnecessary usemtl directives");
     }
 
+    [Test]
+    public void DoNotCreateMtlIfNoMaterial()
+    {
+        // create RvmStore
+        var primitive = new RvmBox(1, Matrix4x4.Identity, new RvmBoundingBox(-Vector3.One, Vector3.One), 2, 2, 2);
+
+        var mesh = TessellatorBridge.Tessellate(primitive, 0.01f);
+        // export
+        var tempFileName = Path.GetTempFileName();
+        var matFileName = Path.ChangeExtension(tempFileName, "mtl");
+        using (var objExporter = new ObjExporter(tempFileName))
+        {
+            objExporter.StartGroup("mesh");
+            objExporter.WriteMesh(mesh);
+        }
+
+        var objContent = File.ReadLines(tempFileName);
+        var materialRefCount = objContent.Count(s => s.StartsWith("usemtl "));
+        var materialFileExists = File.Exists(matFileName);
+        File.Delete(tempFileName);
+
+        // check that mtl file was not created
+        Assert.That(materialFileExists.Equals(false), "MTL file should not be created if no material is specified");
+        // check material count
+        Assert.That(materialRefCount.Equals(0), "OBJ Exporter should not reference non-existing material file");
+    }
 }
