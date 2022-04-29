de1055747d2feab234e2f5f91f40cdf70e653f01
Nils Henrik Hals <nih@equinor.com>
2022-04-29 11:15:14 +0200

    Initial Setup of a SceneSectorAnalyzer and Tests
    Co-authored-by: Anders E Aabakken <aeaa@equinor.com>

---
+++ CadRevealComposer.Tests/Analyzers/SceneSectorAnalyzerTests.cs
@@ -0,0 +1,72 @@
+﻿namespace CadRevealComposer.Tests.Analyzers;
+
+using CadRevealComposer.Analyzers;
+using CadRevealComposer.Tests.Utils;
+using Newtonsoft.Json;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+[TestFixture]
+public class SceneSectorAnalyzerTests
+{
+    Scene scene;
+
+    [OneTimeSetUp]
+    public void OneTimeSetup()
+    {
+        scene = TestSampleLoader.LoadTestJson<Scene>("scene_hda_v8.json");
+    }
+
+    [Test]
+    public void AnalyzeSectors_Returns_SectorCount_AsResult()
+    {
+        SceneSectorAnalyzer.SectorAnalysisResult result = SceneSectorAnalyzer.AnalyzeSectorsInScene(scene);
+        Assert.That(result.SectorCount, Is.EqualTo(119));
+    }
+
+    [Test]
+    public void AnalyzeSectors_Returns_MaxSectorDepth_AsResult()
+    {
+        SceneSectorAnalyzer.SectorAnalysisResult result = SceneSectorAnalyzer.AnalyzeSectorsInScene(scene);
+        Assert.That(result.MaxSectorDepth, Is.EqualTo(5));
+    }
+
+
+    [Test]
+    public void AnalyzeSectors_Returns_AverageSectorCost_AsResult()
+    {
+        SceneSectorAnalyzer.SectorAnalysisResult result = SceneSectorAnalyzer.AnalyzeSectorsInScene(scene);
+        Assert.That(result.AverageEstimatedTriangleCount, Is.EqualTo(50928.4).Within(0.1));
+    }
+
+    [Test]
+    public void AnalyzeSectors_Returns_AverageDrawcallCount_AsResult()
+    {
+        SceneSectorAnalyzer.SectorAnalysisResult result = SceneSectorAnalyzer.AnalyzeSectorsInScene(scene);
+        Assert.That(result.AverageEstimatedDrawcallCount, Is.EqualTo(184.6).Within(0.1));
+    }
+
+
+    [Test]
+    public void AnalyzeSectors_Returns_SumOfSectorCosts_ByPath()
+    {
+        SceneSectorAnalyzer.SectorTreeNode[] result = SceneSectorAnalyzer.GenerateSectorTree(scene.Sectors);
+        Assert.That(result, Has.One.Items);
+    }
+
+    [Test]
+    public void SectorTreeNode_IsLeaf_WhenItHasZeroChildren()
+    {
+        var treeNode = new SceneSectorAnalyzer.SectorTreeNode(
+            Children: Array.Empty<SceneSectorAnalyzer.SectorTreeNode>(),
+            new Sector());
+        Assert.That(treeNode.IsLeaf, Is.True);
+        var treeNode2 = new SceneSectorAnalyzer.SectorTreeNode(Children: new[] { treeNode }, new Sector());
+        Assert.That(treeNode2.IsLeaf, Is.False);
+    }
+
+}
---
+++ CadRevealComposer/Analyzers/SceneSectorAnalyzer.cs
@@ -0,0 +1,61 @@
+﻿namespace CadRevealComposer.Analyzers;
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class SceneSectorAnalyzer
+{
+    public record SectorAnalysisResult(int SectorCount, long MaxSectorDepth, double AverageEstimatedTriangleCount, double AverageEstimatedDrawcallCount);
+
+    public static SectorAnalysisResult AnalyzeSectorsInScene(Scene scene)
+    {
+        var sectors = scene.Sectors.ToImmutableArray();
+        var maxDepth = sectors.Max(x => x.Depth);
+        var averageEstimatedTriangleCount = sectors.Average(x => x.EstimatedTriangleCount);
+        var averageDrawcallCount = sectors.Average(x => x.EstimatedDrawCallCount);
+
+        return new SectorAnalysisResult(
+            SectorCount: scene.Sectors.Length,
+            MaxSectorDepth: maxDepth,
+            AverageEstimatedTriangleCount: averageEstimatedTriangleCount,
+            AverageEstimatedDrawcallCount: averageDrawcallCount);
+    }
+
+
+    public record SectorTreeNode(SectorTreeNode[] Children, Sector Self)
+    {
+        public bool IsLeaf => !Children.Any();
+    }
+    /// <summary>
+    /// Generate a tree based on a list of sectors.
+    /// This returns the "Root" nodes (can be multiple),
+    /// and you have to traverse the tree yourself
+    /// </summary>
+    /// <param name="sectors"></param>
+    /// <returns></returns>
+    /// <exception cref="NotImplementedException"></exception>
+    public static SectorTreeNode[] GenerateSectorTree(Sector[] sectors)
+    {
+        var roots = new List<SectorTreeNode>();
+        foreach(Sector sector in sectors.Where(x => x.ParentId == -1))
+        {
+            var treeNode = new SectorTreeNode(MakeTreeRecursive(sector, sectors), Self: sector);
+            roots.Add(treeNode);
+        }
+
+        return roots.ToArray();
+    }
+
+    public static SectorTreeNode[] MakeTreeRecursive(Sector parent, Sector[] datas)
+    {
+        var children = new List<SectorTreeNode>();
+        foreach (var item in datas.Where(x => x.ParentId == parent.Id))
+        {
+            children.Add(new SectorTreeNode(MakeTreeRecursive(item, datas), item));
+        }
+        return children.ToArray();
+    }
+}
