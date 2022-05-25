9fd712a03f0104c547df668cfb81eb6bc6d4353a
Nils Henrik Hals <nih@equinor.com>
2022-05-25 10:00:01 +0200

    Improve incomplete comment

--- CadRevealComposer/SceneCreator.cs
+++ CadRevealComposer/SceneCreator.cs
@@ -81,8 +81,9 @@ public static class SceneCreator
                         FileName: null,
                         DownloadSize: sector.DownloadSize, // Use sector downloadSize (same as reveal when it does not have a faces file)
                         QuadSize: -1, // This is the default value in Reveal v8 when we do not have faces
-                        // CoverageFactors use a hard-coded value to avoid an issue in Reveal where this is hardcoded to -1. -1 causes the
-                        CoverageFactors: new CoverageFactors(Xy: 0.7f, Xz: 0.7f, Yz: 0.7f), // 0.7 is an arbitrary number (Range of 0-1 of how much of this sector is covered by 3D data)
+                        // CoverageFactors use a hard-coded value to avoid an issue in Reveal where this is hardcoded to -1, -1, -1
+                        // which causes the reveal loading code to assume "100%" coverage, and never loading sectors within other sectors.
+                        CoverageFactors: new CoverageFactors(Xy: 0.7f, Xz: 0.7f, Yz: 0.7f), // 0.7 is an arbitrary number assuming 70% coverage (Range of 0-1 of how much of this sector is covered by 3D data)
                         RecursiveCoverageFactors: null),
                 EstimatedTriangleCount = sector.EstimatedTriangleCount,
                 EstimatedDrawCallCount = sector.EstimatedDrawCallCount
