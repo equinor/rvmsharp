c98193c4e33e03fe4c09704f317b9e946560fc76
Veronika Solteszova <ves@equinor.com>
2022-12-14 13:40:41 +0100

    Fix link in project settings

--- fbximport/tests/tests.cpp
+++ fbximport/tests/tests.cpp
@@ -56,14 +56,14 @@ void load_and_iterate() {
     manager_destroy(sdk);
 }
 
-//TEST_CASE( "Factorials are computed", "[factorial]" ) {
-//
-//    REQUIRE( Factorial(1) == 1 );
-//    REQUIRE( Factorial(2) == 2 );
-//    REQUIRE( Factorial(3) == 6 );
-//    REQUIRE( Factorial(10) == 3628800 );
-//}
+TEST_CASE( "Factorials are computed", "[factorial]" ) {
 
-//TEST_CASE( "Load and iterate", "[FBX sdk]") {
-//    load_and_iterate();
-//}
+    REQUIRE( Factorial(1) == 1 );
+    REQUIRE( Factorial(2) == 2 );
+    REQUIRE( Factorial(3) == 6 );
+    REQUIRE( Factorial(10) == 3628800 );
+}
+
+TEST_CASE( "Load and iterate", "[FBX sdk]") {
+    load_and_iterate();
+}
