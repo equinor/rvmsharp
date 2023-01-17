e147a22d42f901f5f469d3eb1bcdede2411367a0
Nils Henrik Hals <3185998+Strepto@users.noreply.github.com>
2023-01-17 10:51:37 +0100

    Update comment

--- RvmSharp/Primitives/RvmLine.cs
+++ RvmSharp/Primitives/RvmLine.cs
@@ -4,6 +4,12 @@ using System.Numerics;
 
 /// <summary>
 /// RvmLine is not yet well understood. Contributions welcome.
+///
+/// NIH: The RvmLines I have found were all very strange. The A and B data could maybe be a description of the line,
+/// but I'm not sure how to handle them. The RvmLines I have found in Echo data are A=5 and B=0.
+///
+/// A theory is that A and B are the X positions in a vec3, and that the line is aligned with the matrix. In the data
+/// I found that did not align with the Bounding Box data. So something is weird.
 /// </summary>
 /// <param name="Version"></param>
 /// <param name="Matrix"></param>
