b3f9f4ba0e881b1bb373a46bc3018ce1f060a505
Nils Henrik Hals <3185998+Strepto@users.noreply.github.com>
2023-01-17 12:24:09 +0100

    Add test

---
+++ CadRevealComposer.Tests/Utils/MeshTools/MeshToolsTests.cs
@@ -0,0 +1,24 @@
+﻿namespace CadRevealComposer.Tests.Utils.MeshTools;
+
+using CadRevealFbxProvider.BatchUtils;
+using NUnit.Framework;
+using RvmSharp.Tessellation;
+using System.Numerics;
+
+[TestFixture]
+public class MeshToolsTests
+{
+    [Test]
+    public void Test_DeduplicateVertices()
+    {
+        // Sample data is not a valid mesh, but tests logic
+        var mesh = new Mesh(new[] { Vector3.One, Vector3.Zero, Vector3.One }, null, new[] { 0, 1, 2, 1, 0, 2 }, 0);
+        var newMesh =  MeshTools.DeduplicateVertices(mesh);
+        Assert.That(newMesh.Vertices, Is.EquivalentTo(new []{ Vector3.One, Vector3.Zero }));
+
+        // Expect Vertex 0 and 2 to be equal, and that indices have been changed to only be 0
+        Assert.That(newMesh.Triangles, Is.EquivalentTo(new []{ 0, 1, 0, 1, 0, 0 }));
+        // Expect normals to be discarded, as they usually follow triangles.
+        Assert.That(newMesh.Normals, Is.Null);
+    }
+}
