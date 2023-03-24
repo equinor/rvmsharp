19abe06dc979bac39c95a5a5fbb2a3b1673445f9
Veronika Solteszova <ves@equinor.com>
2023-03-24 10:28:57 +0100

    Change attribute parsing to reflect the new template attribute file

--- CadRevealFbxProvider/Attributes/ScaffoldingAttributeParser.cs
+++ CadRevealFbxProvider/Attributes/ScaffoldingAttributeParser.cs
@@ -8,22 +8,38 @@ public class ScaffoldingAttributeParser
     {
         // TODO: Add unit tests
         // TODO: Refactor ParseAttributes
-        var data = CsvReader.ReadFromText(String.Join(Environment.NewLine, fileLines), new CsvOptions()
+        var attributeRawData = CsvReader.ReadFromText(String.Join(Environment.NewLine, fileLines), new CsvOptions()
         {
             HeaderMode = HeaderMode.HeaderPresent,
-            RowsToSkip = 1,
+            RowsToSkip = 0,
+            SkipRow = (ReadOnlyMemory<char> row, int idx) => row.Span.IsEmpty || row.Span[0] == '#' || idx == 2,
             TrimData = true,
             Separator = ';'
         });
 
-        var idNummerCol = Array.IndexOf(
-            data.First().Headers, "Equinor ID nummer");
-        //var datas = data.ToDictionary(x => x.Values[0], v =>
-        var datas = data.ToDictionary(x => x.Values[idNummerCol], v =>
+//<<<<<<< HEAD
+//        var idNummerCol = Array.IndexOf(
+//            data.First().Headers, "Equinor ID nummer");
+//        //var datas = data.ToDictionary(x => x.Values[0], v =>
+//        var datas = data.ToDictionary(x => x.Values[idNummerCol], v =>
+//        {
+//            var kvp = new Dictionary<string, string>();
+//            for (int col = 0; col < v.ColumnCount ; col++)
+//=======
+        var indexIdColumn = Array.IndexOf(attributeRawData.First().Headers, "Item code");
+        //int keyHeaderIndex = attributeRawData.First().ColumnCount - 1;
+        //var keyHeader = attributeRawData.First().Headers[keyHeaderIndex];
+        if (indexIdColumn < 0)
+            throw new Exception("Key header \"Item code\" is missing in the attribute file.");
+
+        var attribtuesDictionary = attributeRawData.ToDictionary(x => x.Values[indexIdColumn], v =>
         {
             var kvp = new Dictionary<string, string>();
-            for (int col = 0; col < v.ColumnCount ; col++)
+
+            for (int col = 0; col < v.ColumnCount; col++)
             {
+                if (indexIdColumn == col) continue;
+
                 var header = v.Headers[col];
                 var value = v.Values[col];
                 kvp[header] =  value;
@@ -32,6 +48,6 @@ public class ScaffoldingAttributeParser
             return kvp;
         });
 
-        return datas;
+        return attribtuesDictionary;
     }
 }
