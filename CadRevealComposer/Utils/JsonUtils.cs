6190ce826a8a517ef6c409c98551db9b00c5e8c2
Nils Henrik Hals <3185998+Strepto@users.noreply.github.com>
2023-04-18 15:52:34 +0200

    Cleanup serializing of camera edge case

--- CadRevealComposer/Utils/JsonUtils.cs
+++ CadRevealComposer/Utils/JsonUtils.cs
@@ -1,8 +1,9 @@
 namespace CadRevealComposer.Utils;
 
 using Newtonsoft.Json;
-using Operations;
 using System.IO;
+using System.Text.Json;
+using JsonSerializer = System.Text.Json.JsonSerializer;
 
 public static class JsonUtils
 {
@@ -11,33 +12,6 @@ public static class JsonUtils
         using var stream = File.Create(filename);
         using var writer = new StreamWriter(stream);
         using var jsonWriter = new JsonTextWriter(writer);
-        var jsonSerializer = new JsonSerializer { Formatting = formatting };
-        jsonSerializer.Serialize(jsonWriter, obj);
-    }
-
-    public static void JsonSerializeToFile(CameraPositioning.CameraPosition cameraPosition, string filePath)
-    {
-        var json = JsonConvert.SerializeObject(new
-        {
-            cameraPosition = new
-            {
-                x = cameraPosition.Position.X,
-                y = cameraPosition.Position.Y,
-                z = cameraPosition.Position.Z
-            },
-            cameraDirection = new
-            {
-                x = cameraPosition.Direction.X,
-                y = cameraPosition.Direction.Y,
-                z = cameraPosition.Direction.Z
-            },
-            targetPosition = new
-            {
-                x = cameraPosition.Target.X,
-                y = cameraPosition.Target.Y,
-                z = cameraPosition.Target.Z
-            }
-        }, Formatting.Indented);
-        File.WriteAllText(filePath, json);
+        JsonSerializer.Serialize(stream, obj, new JsonSerializerOptions(JsonSerializerDefaults.Web){ WriteIndented = formatting == Formatting.Indented});
     }
 }
