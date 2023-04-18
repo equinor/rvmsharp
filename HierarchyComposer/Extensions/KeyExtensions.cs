dace970b88f46a0c2b7075dd2577af64c203d4f5
Nils Henrik Hals <3185998+Strepto@users.noreply.github.com>
2023-04-18 23:07:42 +0200

    Commenting

--- HierarchyComposer/Extensions/KeyExtensions.cs
+++ HierarchyComposer/Extensions/KeyExtensions.cs
@@ -7,6 +7,7 @@ public static class KeyExtensions
 {
     public static string GetGroupKey(this AABB aabb)
     {
+        // Intentionally reduces precision of the group key to allow for more deduplication
         return $"{aabb.min.x:0.00},{aabb.min.y:0.00},{aabb.min.z:0.00}" +
                $",{aabb.max.x:0.00},{aabb.max.y:0.00},{aabb.max.z:0.00}";
     }
