3172bfa59c980a56625586631e6afc2e3ca72f9a
Eirik Fosse <eif@equinor.com>
2023-06-09 10:56:29 +0200

    Add support for storing node id in vertex color channels of meshes

--- RvmSharp.Exe/Options.cs
+++ RvmSharp.Exe/Options.cs
@@ -5,12 +5,13 @@
 
     internal class Options
     {
-        public Options(IEnumerable<string> inputs, string filter, string output, float tolerance)
+        public Options(IEnumerable<string> inputs, string filter, string output, float tolerance, uint nodeId)
         {
             Inputs = inputs;
             Filter = filter;
             Output = output;
             Tolerance = tolerance;
+            NodeId = nodeId;
         }
 
         [Option('i', "input", Required = true, HelpText = "Input file or folder, can specify multiple items")]
@@ -24,5 +25,8 @@
 
         [Option('t', "tolerance", Default = 0.1f, Required = false, HelpText = "Tessellation tolerance")]
         public float Tolerance { get; }
+        
+        [Option('n', "nodeid", Default = null, Required = false, HelpText = "Node ID to start counting from, if this parameter is set Node Id will be stored in vertex color channels of the meshes")]
+        public uint? NodeId { get; }
     }
 }
--- RvmSharp/Tessellation/Mesh.cs
+++ RvmSharp/Tessellation/Mesh.cs
@@ -8,6 +8,7 @@
     {
         public readonly float Error;
         public readonly Vector3[] Vertices;
+        public Vector3[] VertexColors;
         public readonly Vector3[] Normals;
         public readonly int[] Triangles;
 
@@ -19,6 +20,8 @@
 
             Vertices = new Vector3[vertexData.Length / 3];
             Normals = new Vector3[normalData.Length / 3];
+            VertexColors = Array.Empty<Vector3>();
+
             for (var i = 0; i < vertexData.Length / 3; i++)
             {
                 Vertices[i] = new Vector3(vertexData[i * 3], vertexData[i * 3 + 1], vertexData[i * 3 + 2]);
@@ -38,6 +41,22 @@
             Vertices = vertices;
             Normals = normals;
             Triangles = triangles;
+            VertexColors = Array.Empty<Vector3>();
+        }
+
+        public Mesh(Vector3[] vertices, Vector3[] normals, int[] triangles, Vector3[] vertexColors, float error)
+        {
+            if (vertices.Length != normals.Length)
+                throw new ArgumentException("Vertex and normal arrays must have equal length");
+
+            if (vertices.Length != vertexColors.Length)
+                throw new ArgumentException("Vertex and vertex color arrays must have equal length");
+
+            Error = error;
+            Vertices = vertices;
+            Normals = normals;
+            Triangles = triangles;
+            VertexColors = vertexColors;
         }
 
         public void Apply(Matrix4x4 matrix)
@@ -49,14 +68,33 @@
             }
         }
 
+        public void ApplySingleColor(uint value)
+        {
+            if (value > 0xffffff)
+            {
+                throw new ArgumentOutOfRangeException(nameof(value), $"Value must fit in 24 bits, max value is {0xffffff}");
+            }
+            
+            var x = (value & 0xff0000) >> 16;
+            var y = (value & 0x00ff00) >> 8;
+            var z = (value & 0x0000ff);
+            var xf = (x + 0.1f) / 255f;    /////
+            var yf = (y + 0.1f) / 255f;    // Add 0.1 to avoid floor rounding errors in float to int conversion in Unreal
+            var zf = (z + 0.1f) / 255f;    /////
+            var color = new Vector3(xf, yf, zf);
+            VertexColors = new Vector3[Vertices.Length];
+            Array.Fill(VertexColors, color);
+        }
+
         public static Mesh Merge(Mesh mesh1, Mesh mesh2)
         {
             var mesh1VertexCount = mesh1.Vertices.Length;
             var vertices = mesh1.Vertices.Concat(mesh2.Vertices).ToArray();
             var normals = mesh1.Normals.Concat(mesh2.Normals).ToArray();
             var triangles = mesh1.Triangles.Concat(mesh2.Triangles.Select(t => t + mesh1VertexCount)).ToArray();
+            var vertexColors = mesh1.VertexColors.Concat(mesh2.VertexColors).ToArray();
             var error = Math.Max(mesh1.Error, mesh2.Error);
-            return new Mesh(vertices, normals, triangles, error);
+            return vertexColors.Length > 0 ? new Mesh(vertices, normals, triangles, vertexColors, error) : new Mesh(vertices, normals, triangles, error);
         }
     }
 }
