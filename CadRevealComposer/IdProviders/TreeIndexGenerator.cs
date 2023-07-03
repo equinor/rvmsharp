4edda1b66f9d9400523a58f852701e483dd9f694
Nils Henrik Hals <3185998+Strepto@users.noreply.github.com>
2023-07-03 15:08:23 +0200

    Make TreeIndexes start from 1
    This allows Zero (0) being reserved as non-existing

--- CadRevealComposer/IdProviders/TreeIndexGenerator.cs
+++ CadRevealComposer/IdProviders/TreeIndexGenerator.cs
@@ -1,18 +1,27 @@
 namespace CadRevealComposer.IdProviders;
 
+using System.Diagnostics;
+
+/// <summary>
+/// A Sequential Id Generator for TreeIndexes, Indexes here start from 1
+///
+///
+/// Its just a sequential generator for now. It assumes that all nodes are added depth first!
+/// Example tree with indexes (root is left, leaf is right):
+/// 1
+///   2
+///     3
+///     4
+///   5
+///     6
+///       7
+/// 8
+/// </summary>
 public class TreeIndexGenerator : SequentialIdGenerator
 {
-    // Separate empty implementations SequentialIdGenerator are used to
-    // avoid a TreeIndexGenerator being sent in where a InstanceIdGenerator or a NodeIdGenerator should be used.
-
-    // Its just a sequential generator for now. It assumes that all nodes are added depth first!
-    // Example tree with indexes (root is left, leaf is right):
-    // 1
-    //   2
-    //     3
-    //     4
-    //   5
-    //     6
-    //       7
-    // 8
+    public TreeIndexGenerator()
+    {
+        var id = GetNextId(); // Always start TreeIndexes at 1 as the database uses Zero non-existing.
+        Debug.Assert(id == 0);
+    }
 }
