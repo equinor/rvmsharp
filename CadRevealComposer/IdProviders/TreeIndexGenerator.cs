0d4d69ce549624cc58de410737277a368a972d48
Nils Henrik Hals <3185998+Strepto@users.noreply.github.com>
2023-07-03 15:39:05 +0200

    TreeIndexes start from1 (#150)
    This allows Zero (0) being reserved as non-existing in hierarchy service

--- CadRevealComposer/IdProviders/TreeIndexGenerator.cs
+++ CadRevealComposer/IdProviders/TreeIndexGenerator.cs
@@ -1,18 +1,25 @@
 namespace CadRevealComposer.IdProviders;
 
+/// <summary>
+/// A Sequential Id Generator for TreeIndexes, Indexes here start from 1
+///
+///
+/// Its just a sequential generator for now. It assumes that all nodes are added depth first!
+/// Example tree with indexes (root is left, leaf is right):
+/// 1
+///   2
+///     3
+///     4
+///   5
+///     6
+///       7
+/// 8
+/// </summary>
 public class TreeIndexGenerator : SequentialIdGenerator
 {
-    // Separate empty implementations SequentialIdGenerator are used to
-    // avoid a TreeIndexGenerator being sent in where a InstanceIdGenerator or a NodeIdGenerator should be used.
-
-    // Its just a sequential generator for now. It assumes that all nodes are added depth first!
-    // Example tree with indexes (root is left, leaf is right):
-    // 1
-    //   2
-    //     3
-    //     4
-    //   5
-    //     6
-    //       7
-    // 8
+    public TreeIndexGenerator()
+    {
+        // "Pre-increment" the first id (which is zero) to avoid that being used anywhere else.
+        _ = GetNextId(); // This returns zero, but we discard it.
+    }
 }
