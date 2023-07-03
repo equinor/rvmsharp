38d564de5cab556bb134bf71a367002b535f2938
Nils Henrik Hals <3185998+Strepto@users.noreply.github.com>
2023-07-03 15:11:07 +0200

    Add tests

---
+++ CadRevealComposer.Tests/IdGenerators/TreeIndexGeneratorTests.cs
@@ -0,0 +1,24 @@
+﻿namespace CadRevealComposer.Tests.IdGenerators;
+
+using IdProviders;
+
+public class TreeIndexGeneratorTests
+{
+    [Test]
+    public void TreeIndex_Generator_WhenNew_GivesFirstIndexAs1()
+    {
+        var tig = new TreeIndexGenerator();
+        var firstIndex = tig.GetNextId();
+        Assert.That(firstIndex, Is.EqualTo(1));
+    }
+
+    [Test]
+    public void TreeIndex_Generator_IteratesToNextIdEveryTimeANewIdIsRequested()
+    {
+        var tig = new TreeIndexGenerator();
+        var firstIndex = tig.GetNextId();
+        Assert.That(firstIndex, Is.EqualTo(1));
+        var secondIndex = tig.GetNextId();
+        Assert.That(secondIndex, Is.EqualTo(2));
+    }
+}
