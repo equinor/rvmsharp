6251595c39822f16f2c21e33cd8fda7dede03c47
Nils Henrik Hals <3185998+Strepto@users.noreply.github.com>
2023-07-03 15:12:58 +0200

    Add "Why" comment

--- CadRevealComposer.Tests/IdGenerators/TreeIndexGeneratorTests.cs
+++ CadRevealComposer.Tests/IdGenerators/TreeIndexGeneratorTests.cs
@@ -7,6 +7,7 @@ public class TreeIndexGeneratorTests
     [Test]
     public void TreeIndex_Generator_WhenNew_GivesFirstIndexAs1()
     {
+        // The Hierarchy Database uses zero 0 as a special number, so we keep this scheme here as well. Starting always from 1.
         var tig = new TreeIndexGenerator();
         var firstIndex = tig.GetNextId();
         Assert.That(firstIndex, Is.EqualTo(1));
