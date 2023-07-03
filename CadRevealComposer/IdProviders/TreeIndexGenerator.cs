33d155383e1cde5d35a7458ecef97f5ad8c064ff
Nils Henrik Hals <3185998+Strepto@users.noreply.github.com>
2023-07-03 15:17:51 +0200

    Fix feedback

--- CadRevealComposer/IdProviders/TreeIndexGenerator.cs
+++ CadRevealComposer/IdProviders/TreeIndexGenerator.cs
@@ -1,7 +1,5 @@
 namespace CadRevealComposer.IdProviders;
 
-using System.Diagnostics;
-
 /// <summary>
 /// A Sequential Id Generator for TreeIndexes, Indexes here start from 1
 ///
@@ -21,7 +19,7 @@ public class TreeIndexGenerator : SequentialIdGenerator
 {
     public TreeIndexGenerator()
     {
-        var id = GetNextId(); // Always start TreeIndexes at 1 as the database uses Zero non-existing.
-        Debug.Assert(id == 0);
+        // "Pre-increment" the first id (which is zero) to avoid that being used anywhere else.
+        _ = GetNextId(); // This returns zero, but we discard it.
     }
 }
