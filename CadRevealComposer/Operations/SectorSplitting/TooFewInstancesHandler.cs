45d9739e53678d522a51c1d2c6c91c942f0ba1f5
Vegard Stengrundet (Omega AS) <VEGST@equinor.com>
2023-07-18 08:53:35 +0200

    Update comments

--- CadRevealComposer/Operations/SectorSplitting/TooFewInstancesHandler.cs
+++ CadRevealComposer/Operations/SectorSplitting/TooFewInstancesHandler.cs
@@ -3,10 +3,15 @@ namespace CadRevealComposer.Operations.SectorSplitting;
 using Primitives;
 using System.Linq;
 
-// Convert instances to mesh to avoid too much batching
-// See spike document TODO: Update after spike document merge
+/// <summary>
+/// If there are too few instances of a template in a sector we assume that the cost of batching it is greater than the reward.
+/// The batching is done in Reveal and each type of template in each sector will create a separate batch.
+/// This can be avoided by converting to TriangleMesh.
+/// </summary>
 public class TooFewInstancesHandler
 {
+    // Minimum number of a instances of a template in a sector, otherwise convert to mesh
+    // See spike document for data of what this number means TODO: Update after spike document merge
     private const int NumberOfInstancesThreshold = 10; // Minimum number of instances, otherwise convert to mesh
 
     public APrimitive[] ConvertInstancesWhenTooFew(APrimitive[] geometries)
