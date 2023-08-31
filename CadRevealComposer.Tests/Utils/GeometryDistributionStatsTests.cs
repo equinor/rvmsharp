319b96ed18d4f4be81ad358541e8644032634472
Nils Henrik Hals <3185998+Strepto@users.noreply.github.com>
2023-08-31 16:06:11 +0200

    Create GeometryDistributionStats helper

---
+++ CadRevealComposer.Tests/Utils/GeometryDistributionStatsTests.cs
@@ -0,0 +1,65 @@
+﻿namespace CadRevealComposer.Tests.Utils;
+
+using CadRevealComposer.Utils;
+using Primitives;
+
+public class GeometryDistributionStatsTests
+{
+    [Test]
+    public void Constructor_CountsPrimitivesCorrectly()
+    {
+        // Arrange
+        var primitives = new APrimitive[]
+        {
+            // ReSharper disable AssignNullToNotNullAttribute -- Not important in this test
+            new Box(default, default, default, default),
+            new Cone(default, default, default, default, default, default, default, default, default, default),
+            new Cone(default, default, default, default, default, default, default, default, default, default),
+            new EllipsoidSegment(default, default, default, default, default, default, default, default),
+            new EllipsoidSegment(default, default, default, default, default, default, default, default),
+            new TriangleMesh(default, default, default, default),
+            new InstancedMesh(default, default, default, default, default, default),
+            new InstancedMesh(default, default, default, default, default, default),
+            new Circle(default, default, default, default, default),
+            new Trapezium(default, default, default, default, default, default, default),
+            new Trapezium(default, default, default, default, default, default, default),
+            new Nut(default, default, default, default),
+            new GeneralRing(default, default, default, default, default, default, default, default),
+            new Quad(default, default, default, default),
+            new GeneralCylinder(
+                default,
+                default,
+                default,
+                default,
+                default,
+                default,
+                default,
+                default,
+                default,
+                default,
+                default
+            ),
+            new TorusSegment(default, default, default, default, default, default, default),
+            new EccentricCone(default, default, default, default, default, default, default, default)
+            // ReSharper enable AssignNullToNotNullAttribute
+        };
+
+        // Act
+        var distribution = new GeometryDistributionStats(primitives);
+
+        // Assert
+        Assert.AreEqual(1, distribution.Boxes);
+        Assert.AreEqual(1, distribution.Circles);
+        Assert.AreEqual(2, distribution.Cones);
+        Assert.AreEqual(1, distribution.EccentricCones);
+        Assert.AreEqual(2, distribution.EllipsoidSegments);
+        Assert.AreEqual(1, distribution.GeneralCylinders);
+        Assert.AreEqual(1, distribution.GeneralRings);
+        Assert.AreEqual(2, distribution.InstancedMeshes);
+        Assert.AreEqual(1, distribution.Nuts);
+        Assert.AreEqual(1, distribution.Quads);
+        Assert.AreEqual(1, distribution.TorusSegments);
+        Assert.AreEqual(2, distribution.Trapeziums);
+        Assert.AreEqual(1, distribution.TriangleMeshes);
+    }
+}
---
+++ CadRevealComposer/Utils/GeometryDistributionStats.cs
@@ -0,0 +1,74 @@
+﻿namespace CadRevealComposer.Utils;
+
+using Primitives;
+using System;
+using System.Collections.Generic;
+
+[Serializable]
+public class GeometryDistributionStats
+{
+    public int Boxes { get; }
+    public int Circles { get; }
+    public int Cones { get; }
+    public int EccentricCones { get; }
+    public int EllipsoidSegments { get; }
+    public int GeneralCylinders { get; }
+    public int GeneralRings { get; }
+    public int Nuts { get; }
+    public int Quads { get; }
+    public int TorusSegments { get; }
+    public int Trapeziums { get; }
+    public int InstancedMeshes { get; }
+    public int TriangleMeshes { get; }
+
+    public GeometryDistributionStats(IEnumerable<APrimitive> primitives)
+    {
+        foreach (APrimitive primitive in primitives)
+        {
+            switch (primitive)
+            {
+                case Box:
+                    Boxes++;
+                    break;
+                case Circle:
+                    Circles++;
+                    break;
+                case Cone:
+                    Cones++;
+                    break;
+                case EccentricCone:
+                    EccentricCones++;
+                    break;
+                case EllipsoidSegment:
+                    EllipsoidSegments++;
+                    break;
+                case GeneralCylinder:
+                    GeneralCylinders++;
+                    break;
+                case GeneralRing:
+                    GeneralRings++;
+                    break;
+                case InstancedMesh:
+                    InstancedMeshes++;
+                    break;
+                case Nut:
+                    Nuts++;
+                    break;
+                case Quad:
+                    Quads++;
+                    break;
+                case TorusSegment:
+                    TorusSegments++;
+                    break;
+                case Trapezium:
+                    Trapeziums++;
+                    break;
+                case TriangleMesh:
+                    TriangleMeshes++;
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(primitive));
+            }
+        }
+    }
+}
