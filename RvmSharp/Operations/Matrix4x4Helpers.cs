b688c6110e6e456231f913c328cf482cf09458ca
BenjaminKjopKofoed <141636529+BenjaminKjopKofoed@users.noreply.github.com>
2023-10-19 13:01:56 +0200

    Bug where Oseberg C build fails id:132764 (#176)
    * Add checks for values of infinity or NaN when parsing stream data
    
    * Added a test, removed unused code and formated.
    
    * Added a test checking for matrix containing infinite value

--- RvmSharp/Operations/Matrix4x4Helpers.cs
+++ RvmSharp/Operations/Matrix4x4Helpers.cs
@@ -17,4 +17,34 @@ public static class Matrix4x4Helpers
             * Matrix4x4.CreateFromQuaternion(rotation)
             * Matrix4x4.CreateTranslation(position);
     }
+
+    /// <summary>
+    /// Checks if all values of the matrix are finite. Infinite makes no sense and breaks later code.
+    /// This guard exists due to a issue on Oseberg C which contained 3 nodes with infinite values.
+    /// </summary>
+    public static bool MatrixContainsInfiniteValue(Matrix4x4 matrix)
+    {
+        if (
+            !float.IsFinite(matrix.M11)
+            || !float.IsFinite(matrix.M12)
+            || !float.IsFinite(matrix.M13)
+            || !float.IsFinite(matrix.M14)
+            || !float.IsFinite(matrix.M21)
+            || !float.IsFinite(matrix.M22)
+            || !float.IsFinite(matrix.M23)
+            || !float.IsFinite(matrix.M24)
+            || !float.IsFinite(matrix.M31)
+            || !float.IsFinite(matrix.M32)
+            || !float.IsFinite(matrix.M33)
+            || !float.IsFinite(matrix.M34)
+            || !float.IsFinite(matrix.M41)
+            || !float.IsFinite(matrix.M42)
+            || !float.IsFinite(matrix.M43)
+            || !float.IsFinite(matrix.M44)
+        )
+        {
+            return true;
+        }
+        return false;
+    }
 }
