7103dd95fb3f681ae9002fb011262ba858978714
Benjamin Kjøpstad Kofoed <BKOF@equinor.com>
2023-10-23 15:41:21 +0200

    Added test to check winding order

--- CadRevealComposer.Tests/Operations/Tessellating/BoxTessellatorTests.cs
+++ CadRevealComposer.Tests/Operations/Tessellating/BoxTessellatorTests.cs
@@ -2,6 +2,7 @@
 
 using CadRevealComposer.Operations.Tessellating;
 using CadRevealComposer.Primitives;
+using SharpGLTF.Memory;
 using System.Drawing;
 using System.Numerics;
 
@@ -22,5 +23,50 @@ public class BoxTessellatorTests
 
         Assert.AreEqual(vertices.Length, 8);
         Assert.AreEqual(indices.Length, 36);
+    }
+
+    [Test]
+    public void WindingOrderTest()
+    {
+        var dummyBoundingBox = new BoundingBox(Vector3.Zero, Vector3.Zero);
+
+        var box = new Box(Matrix4x4.Identity, 1, Color.Red, dummyBoundingBox);
+
+        var tessellatedBox = BoxTessellator.Tessellate(box);
+
+        var vertices = tessellatedBox.Mesh.Vertices;
+        var indices = tessellatedBox.Mesh.Indices;
+
+        //   | a d g |
+        //   | b e h |
+        //   | c f i |
+        // determinant = a(ei - fh) - b(di - gf) + c(dh - eg)
+
+        for (uint index = 0; index < indices.Length; index += 3)
+        {
+            uint i1 = indices[index];
+            uint i2 = indices[index + 1];
+            uint i3 = indices[index + 2];
+
+            Vector3 v1 = vertices[i1];
+            Vector3 v2 = vertices[i2];
+            Vector3 v3 = vertices[i3];
+
+            float a = v1.X;
+            float b = v1.Y;
+            float c = v1.Z;
+            float d = v2.X;
+            float e = v2.Y;
+            float f = v2.Z;
+            float g = v3.X;
+            float h = v3.Y;
+            float i = v3.Z;
+            float j = v3.Z;
+            float k = v3.X;
+
+            float determinant = a * (e * i - f * h) - b * (d * i - f * g) + c * (d * h - e * g);
+
+            Assert.GreaterOrEqual(determinant, 0);
+        }
     }
 }
