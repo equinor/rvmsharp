b45e7ce72ee691627025e751dfda93d2879d60ff
vegasten <vegard@kodeworks.no>
2023-10-24 10:37:05 +0200

    Fix winding order in box tessellator

--- CadRevealComposer/Operations/Tessellating/BoxTessellator.cs
+++ CadRevealComposer/Operations/Tessellating/BoxTessellator.cs
@@ -23,11 +23,11 @@ public static class BoxTessellator
         // csharpier-ignore
         var indices = new uint[]
         {
-            0,1,2,
-            0,2,3,
+            0,2,1,
+            0,3,2,
             0,1,5,
             0,5,4,
-            1,6,2,
+            1,2,6,
             1,6,5,
             2,3,7,
             2,7,6,
