fa303bec0dc0707cbb7b304d00931ea0c122454a
Nils Henrik Hals <3185998+Strepto@users.noreply.github.com>
2023-11-10 14:26:10 +0100

    Format file

--- CadRevealComposer/Configuration/ComposerParameters.cs
+++ CadRevealComposer/Configuration/ComposerParameters.cs
@@ -2,11 +2,13 @@ namespace CadRevealComposer.Configuration;
 
 using System.IO;
 
-public record ComposerParameters(bool NoInstancing,
+public record ComposerParameters(
+    bool NoInstancing,
     bool SingleSector,
     bool SplitIntoZones,
     NodeNameExcludeRegex NodeNameExcludeRegex,
     float SimplificationThreshold,
-    DirectoryInfo? DevPrimitiveCacheFolder);
+    DirectoryInfo? DevPrimitiveCacheFolder
+);
 
 public record NodeNameExcludeRegex(string? Value);
