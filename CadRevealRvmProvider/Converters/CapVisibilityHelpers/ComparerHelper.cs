53d4bf9e754201608341cdcc1ff7e4f5be1f61a1
Benjamin Kjøpstad Kofoed <BKOF@equinor.com>
2024-01-24 12:59:51 +0100

    Add xml doc

--- CadRevealRvmProvider/Converters/CapVisibilityHelpers/ComparerHelper.cs
+++ CadRevealRvmProvider/Converters/CapVisibilityHelpers/ComparerHelper.cs
@@ -2,6 +2,15 @@
 
 public static class ComparerHelper
 {
+    /// <summary>
+    ///  Checks if a cap is visible, i.e. check if there are any overlapping caps.
+    /// </summary>
+    /// <param name="checkFirstPrimitive"></param>
+    /// <param name="firstPrimitiveRadius"></param>
+    /// <param name="secondPrimitiveSemiMinorRadius"></param>
+    /// <param name="secondPrimitiveSemiMajorRadius"></param>
+    /// <param name="tolerance"></param>
+    /// <returns>True if there are no overlapping caps. False otherwise.</returns>
     public static bool IsVisible(
         bool checkFirstPrimitive,
         float firstPrimitiveRadius,
@@ -28,23 +37,31 @@ public static class ComparerHelper
         return true;
     }
 
+    /// <summary>
+    /// Checks if a cap is visible, i.e. check if there are any overlapping caps.
+    /// </summary>
+    /// <param name="checkFirstPrimitive"></param>
+    /// <param name="firstPrimitiveRadius"></param>
+    /// <param name="secondPrimitiveRadius"></param>
+    /// <param name="tolerance"></param>
+    /// <returns>True if there are no overlapping caps. False otherwise.</returns>
     public static bool IsVisible(
         bool checkFirstPrimitive,
         float firstPrimitiveRadius,
-        float secondPrimitiveSemiMinorRadius,
+        float secondPrimitiveRadius,
         float tolerance
     )
     {
         if (checkFirstPrimitive)
         {
-            if (secondPrimitiveSemiMinorRadius + tolerance >= firstPrimitiveRadius)
+            if (secondPrimitiveRadius + tolerance >= firstPrimitiveRadius)
             {
                 return false;
             }
         }
         else
         {
-            if (firstPrimitiveRadius + tolerance >= secondPrimitiveSemiMinorRadius)
+            if (firstPrimitiveRadius + tolerance >= secondPrimitiveRadius)
             {
                 return false;
             }
