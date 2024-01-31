1baebd5e38b80b74dbb1700800675c64342a6ae8
Benjamin Kjøpstad Kofoed <BKOF@equinor.com>
2024-01-31 16:14:27 +0100

    Revert back changes to improve readibility

--- CadRevealRvmProvider.Tests/Converters/CapVisibilityTests.cs
+++ CadRevealRvmProvider.Tests/Converters/CapVisibilityTests.cs
@@ -749,7 +749,7 @@ public class CapVisibilityTests
 
         var snout1CapCenter = 0.5f * (new Vector3(snout1.OffsetX, snout1.OffsetY, snout1.Height));
         var snout1CapCenterB = -0.5f * (new Vector3(snout1.OffsetX, snout1.OffsetY, snout1.Height));
-        (var snout1N, _) = GeometryHelper.GetPlaneFromShearAndPoint(
+        (var snout1_n, _) = GeometryHelper.GetPlaneFromShearAndPoint(
             snout1.TopShearX,
             snout1.TopShearY,
             snout1CapCenter
@@ -761,7 +761,7 @@ public class CapVisibilityTests
             1,
             0,
             snout1CapCenter,
-            snout1N,
+            snout1_n,
             RvmConnection.ConnectionType.HasCircularSide
         );
 
@@ -775,6 +775,7 @@ public class CapVisibilityTests
     public void CalculateCapVisibility_TestSnoutCapMatch_RotatedCylinders()
     {
         var transform1 = Matrix4x4.CreateTranslation(0.0f, 0.0f, 0.0f);
+        var rotate = Matrix4x4.CreateRotationX(0.1f);
         var snout1 = new RvmSnout(
             0,
             transform1,
@@ -816,7 +817,7 @@ public class CapVisibilityTests
 
         var snout1CapCenter = 0.5f * (new Vector3(snout1.OffsetX, snout1.OffsetY, snout1.Height));
         var snout1CapCenterB = -0.5f * (new Vector3(snout1.OffsetX, snout1.OffsetY, snout1.Height));
-        (var snout1N, _) = GeometryHelper.GetPlaneFromShearAndPoint(
+        (var snout1_n, _) = GeometryHelper.GetPlaneFromShearAndPoint(
             snout1.TopShearX,
             snout1.TopShearY,
             snout1CapCenter
@@ -828,7 +829,7 @@ public class CapVisibilityTests
             1,
             0,
             snout1CapCenter,
-            snout1N,
+            snout1_n,
             RvmConnection.ConnectionType.HasCircularSide
         );
 
@@ -908,50 +909,56 @@ public class CapVisibilityTests
         var ellipse2 = snout2.GetTopCapEllipse();
 
         var origin = VectorD.Build.Dense(new double[] { 0.0, 0.0, 0.0, 1.0 });
-        var p1El1 = VectorD.Build.Dense(new double[] { ellipse1.Ellipse2DPolar.SemiMajorAxis, 0.0, 0.0, 1.0 });
-        var p2El1 = VectorD.Build.Dense(new double[] { 0.0, ellipse1.Ellipse2DPolar.SemiMinorAxis, 0.0, 1.0 });
-        var p1El2 = VectorD.Build.Dense(new double[] { ellipse2.Ellipse2DPolar.SemiMajorAxis, 0.0, 0.0, 1.0 });
-        var p2El2 = VectorD.Build.Dense(new double[] { 0.0, ellipse2.Ellipse2DPolar.SemiMinorAxis, 0.0, 1.0 });
+        var p1_el1 = VectorD.Build.Dense(new double[] { ellipse1.Ellipse2DPolar.SemiMajorAxis, 0.0, 0.0, 1.0 });
+        var p2_el1 = VectorD.Build.Dense(new double[] { 0.0, ellipse1.Ellipse2DPolar.SemiMinorAxis, 0.0, 1.0 });
+        var p1_el2 = VectorD.Build.Dense(new double[] { ellipse2.Ellipse2DPolar.SemiMajorAxis, 0.0, 0.0, 1.0 });
+        var p2_el2 = VectorD.Build.Dense(new double[] { 0.0, ellipse2.Ellipse2DPolar.SemiMinorAxis, 0.0, 1.0 });
 
-        var ptOrigEll1 = ellipse1.PlaneToModelCoord * origin;
-        var ptOrigEll2 = ellipse2.PlaneToModelCoord * origin;
-        var ptPt1Ell1 = ellipse1.PlaneToModelCoord * p1El1;
-        var ptPt2Ell1 = ellipse1.PlaneToModelCoord * p2El1;
-        var ptPt1Ell2 = ellipse2.PlaneToModelCoord * p1El2;
-        var ptPt2Ell2 = ellipse2.PlaneToModelCoord * p2El2;
+        var pt_orig_ell1 = ellipse1.PlaneToModelCoord * origin;
+        var pt_orig_ell2 = ellipse2.PlaneToModelCoord * origin;
+        var pt_pt1_ell1 = ellipse1.PlaneToModelCoord * p1_el1;
+        var pt_pt2_ell1 = ellipse1.PlaneToModelCoord * p2_el1;
+        var pt_pt1_ell2 = ellipse2.PlaneToModelCoord * p1_el2;
+        var pt_pt2_ell2 = ellipse2.PlaneToModelCoord * p2_el2;
 
         // snout1 -> bottom,
         var snout1CapCenter = -0.5f * (new Vector3(snout1.OffsetX, snout1.OffsetY, snout1.Height));
         //var snout1TopCapCenter4D = new Vector4(snout1.OffsetX, snout1.OffsetY, snout1.Height, 1.0f);
-        (var snout1N, var snout1Dc) = GeometryHelper.GetPlaneFromShearAndPoint(
+        (var snout1_n, var snout1_dc) = GeometryHelper.GetPlaneFromShearAndPoint(
             snout1.BottomShearX,
             snout1.BottomShearY,
             snout1CapCenter
         );
-        var snout1N4D = new Vector4(snout1N.X, snout1N.Y, snout1N.Z, 0.0f);
+        var snout1_n_4d = new Vector4(snout1_n.X, snout1_n.Y, snout1_n.Z, 0.0f);
 
-        var distance10 = snout1N.X * ptOrigEll1[0] + snout1N.Y * ptOrigEll1[1] + snout1N.Z * ptOrigEll1[2] + snout1Dc;
-        var distance11 = snout1N.X * ptPt1Ell1[0] + snout1N.Y * ptPt1Ell1[1] + snout1N.Z * ptPt1Ell1[2] + snout1Dc;
-        var distance12 = snout1N.X * ptPt2Ell1[0] + snout1N.Y * ptPt2Ell1[1] + snout1N.Z * ptPt2Ell1[2] + snout1Dc;
+        var distance1_0 =
+            snout1_n.X * pt_orig_ell1[0] + snout1_n.Y * pt_orig_ell1[1] + snout1_n.Z * pt_orig_ell1[2] + snout1_dc;
+        var distance1_1 =
+            snout1_n.X * pt_pt1_ell1[0] + snout1_n.Y * pt_pt1_ell1[1] + snout1_n.Z * pt_pt1_ell1[2] + snout1_dc;
+        var distance1_2 =
+            snout1_n.X * pt_pt2_ell1[0] + snout1_n.Y * pt_pt2_ell1[1] + snout1_n.Z * pt_pt2_ell1[2] + snout1_dc;
 
         // snout2 -> top,
         var snout2CapCenter = 0.5f * (new Vector3(snout2.OffsetX, snout2.OffsetY, snout2.Height));
         var snout2CapCenterB = -0.5f * (new Vector3(snout2.OffsetX, snout2.OffsetY, snout2.Height));
-        (var snout2N, var snout2Dc) = GeometryHelper.GetPlaneFromShearAndPoint(
+        (var snout2_n, var snout2_dc) = GeometryHelper.GetPlaneFromShearAndPoint(
             snout2.TopShearX,
             snout2.TopShearY,
             snout2CapCenter
         );
-        var snout2N4D = new Vector4(snout2N.X, snout2N.Y, snout2N.Z, 0.0f);
+        var snout2_n_4d = new Vector4(snout2_n.X, snout2_n.Y, snout2_n.Z, 0.0f);
 
-        var distance20 = snout2N.X * ptOrigEll2[0] + snout2N.Y * ptOrigEll2[1] + snout2N.Z * ptOrigEll2[2] + snout2Dc;
-        var distance21 = snout2N.X * ptPt1Ell2[0] + snout2N.Y * ptPt1Ell2[1] + snout2N.Z * ptPt1Ell2[2] + snout2Dc;
-        var distance22 = snout2N.X * ptPt2Ell2[0] + snout2N.Y * ptPt2Ell2[1] + snout2N.Z * ptPt2Ell2[2] + snout2Dc;
+        var distance2_0 =
+            snout2_n.X * pt_orig_ell2[0] + snout2_n.Y * pt_orig_ell2[1] + snout2_n.Z * pt_orig_ell2[2] + snout2_dc;
+        var distance2_1 =
+            snout2_n.X * pt_pt1_ell2[0] + snout2_n.Y * pt_pt1_ell2[1] + snout2_n.Z * pt_pt1_ell2[2] + snout2_dc;
+        var distance2_2 =
+            snout2_n.X * pt_pt2_ell2[0] + snout2_n.Y * pt_pt2_ell2[1] + snout2_n.Z * pt_pt2_ell2[2] + snout2_dc;
 
-        var v4TransfNormal1 = Vector4.Transform(snout1N4D, snout1.Matrix);
-        var v4TransfNormal2 = Vector4.Transform(snout2N4D, snout2.Matrix);
-        var transfNormal1 = new Vector3(v4TransfNormal1.X, v4TransfNormal1.Y, v4TransfNormal1.Z);
-        var transfNormal2 = new Vector3(v4TransfNormal2.X, v4TransfNormal2.Y, v4TransfNormal2.Z);
+        var v4transfNormal1 = Vector4.Transform(snout1_n_4d, snout1.Matrix);
+        var v4transfNormal2 = Vector4.Transform(snout2_n_4d, snout2.Matrix);
+        var transfNormal1 = new Vector3(v4transfNormal1.X, v4transfNormal1.Y, v4transfNormal1.Z);
+        var transfNormal2 = new Vector3(v4transfNormal2.X, v4transfNormal2.Y, v4transfNormal2.Z);
         transfNormal1 = Vector3.Normalize(transfNormal1);
         transfNormal2 = Vector3.Normalize(transfNormal2);
 
@@ -967,22 +974,22 @@ public class CapVisibilityTests
         Matrix4x4 sn1MatInv;
         Matrix4x4.Invert(s1Mat, out sn1MatInv);
 
-        var p1W = Vector4.Transform(snout1CapCenter, s1Mat);
-        var p1M2 = Vector4.Transform(p1W, sn2MatInv);
-        var dC1Pl2 = Vector3.Dot(snout2N, new Vector3(p1M2.X, p1M2.Y, p1M2.Z)) + snout2Dc;
+        var p1_w = Vector4.Transform(snout1CapCenter, s1Mat);
+        var p1_m2 = Vector4.Transform(p1_w, sn2MatInv);
+        var d_c1_pl2 = Vector3.Dot(snout2_n, new Vector3(p1_m2.X, p1_m2.Y, p1_m2.Z)) + snout2_dc;
 
         var p2 = Vector4.Transform(Vector4.Transform(snout2CapCenter, s2Mat), sn1MatInv);
-        var dC2Pl1 = Vector3.Dot(snout1N, new Vector3(p2.X, p2.Y, p2.Z)) + snout1Dc;
+        var d_c2_pl1 = Vector3.Dot(snout1_n, new Vector3(p2.X, p2.Y, p2.Z)) + snout1_dc;
 
-        Assert.AreEqual(0.0, dC1Pl2);
-        Assert.AreEqual(0.0, dC2Pl1);
+        Assert.AreEqual(0.0, d_c1_pl2);
+        Assert.AreEqual(0.0, d_c2_pl1);
 
-        Assert.AreEqual(0.0, distance10 * 1000.0);
-        Assert.AreEqual(0.0, distance11 * 1000.0);
-        Assert.AreEqual(0.0, distance12 * 1000.0);
-        Assert.AreEqual(0.0, distance20 * 1000.0);
-        Assert.AreEqual(0.0, distance21 * 1000.0);
-        Assert.AreEqual(0.0, distance22 * 1000.0);
+        Assert.AreEqual(0.0, distance1_0 * 1000.0);
+        Assert.AreEqual(0.0, distance1_1 * 1000.0);
+        Assert.AreEqual(0.0, distance1_2 * 1000.0);
+        Assert.AreEqual(0.0, distance2_0 * 1000.0);
+        Assert.AreEqual(0.0, distance2_1 * 1000.0);
+        Assert.AreEqual(0.0, distance2_2 * 1000.0);
 
         snout2.Connections[0] = new RvmConnection(
             snout2,
@@ -990,7 +997,7 @@ public class CapVisibilityTests
             1,
             0,
             snout2CapCenter,
-            snout1N,
+            snout1_n,
             RvmConnection.ConnectionType.HasCircularSide
         );
 
@@ -1040,58 +1047,64 @@ public class CapVisibilityTests
             0.0f, // top shear x
             0.0f // top shear y
         );
-        var snout2BottomCap = snout2.GetBottomCapEllipse();
+        var snout2bottomCap = snout2.GetBottomCapEllipse();
 
         Assert.That(snout1TopCap.Ellipse2DPolar.SemiMajorAxis, Is.EqualTo(2.0f));
         Assert.That(snout1TopCap.Ellipse2DPolar.SemiMinorAxis, Is.EqualTo(2.0f));
 
-        Assert.That(snout2BottomCap.Ellipse2DPolar.SemiMajorAxis, Is.EqualTo(2.0 / MathF.Cos(snout2.BottomShearX)));
-        Assert.That(snout2BottomCap.Ellipse2DPolar.SemiMinorAxis, Is.EqualTo(2.0));
+        Assert.That(snout2bottomCap.Ellipse2DPolar.SemiMajorAxis, Is.EqualTo(2.0 / MathF.Cos(snout2.BottomShearX)));
+        Assert.That(snout2bottomCap.Ellipse2DPolar.SemiMinorAxis, Is.EqualTo(2.0));
 
         var origin = VectorD.Build.Dense(new double[] { 0.0, 0.0, 0.0, 1.0 });
-        var p1El1 = VectorD.Build.Dense(new double[] { snout1TopCap.Ellipse2DPolar.SemiMajorAxis, 0.0, 0.0, 1.0 });
-        var p2El1 = VectorD.Build.Dense(new double[] { 0.0, snout1TopCap.Ellipse2DPolar.SemiMinorAxis, 0.0, 1.0 });
-        var p1El2 = VectorD.Build.Dense(new double[] { snout2BottomCap.Ellipse2DPolar.SemiMajorAxis, 0.0, 0.0, 1.0 });
-        var p2El2 = VectorD.Build.Dense(new double[] { 0.0, snout2BottomCap.Ellipse2DPolar.SemiMinorAxis, 0.0, 1.0 });
+        var p1_el1 = VectorD.Build.Dense(new double[] { snout1TopCap.Ellipse2DPolar.SemiMajorAxis, 0.0, 0.0, 1.0 });
+        var p2_el1 = VectorD.Build.Dense(new double[] { 0.0, snout1TopCap.Ellipse2DPolar.SemiMinorAxis, 0.0, 1.0 });
+        var p1_el2 = VectorD.Build.Dense(new double[] { snout2bottomCap.Ellipse2DPolar.SemiMajorAxis, 0.0, 0.0, 1.0 });
+        var p2_el2 = VectorD.Build.Dense(new double[] { 0.0, snout2bottomCap.Ellipse2DPolar.SemiMinorAxis, 0.0, 1.0 });
 
-        var ptOrigEll1 = snout1TopCap.PlaneToModelCoord * origin;
-        var ptOrigEll2 = snout2BottomCap.PlaneToModelCoord * origin;
-        var ptPt1Ell1 = snout1TopCap.PlaneToModelCoord * p1El1;
-        var ptPt2Ell1 = snout1TopCap.PlaneToModelCoord * p2El1;
-        var ptPt1Ell2 = snout2BottomCap.PlaneToModelCoord * p1El2;
-        var ptPt2Ell2 = snout2BottomCap.PlaneToModelCoord * p2El2;
+        var pt_orig_ell1 = snout1TopCap.PlaneToModelCoord * origin;
+        var pt_orig_ell2 = snout2bottomCap.PlaneToModelCoord * origin;
+        var pt_pt1_ell1 = snout1TopCap.PlaneToModelCoord * p1_el1;
+        var pt_pt2_ell1 = snout1TopCap.PlaneToModelCoord * p2_el1;
+        var pt_pt1_ell2 = snout2bottomCap.PlaneToModelCoord * p1_el2;
+        var pt_pt2_ell2 = snout2bottomCap.PlaneToModelCoord * p2_el2;
 
         // snout1 -> top
         var snout1CapCenter = 0.5f * (new Vector3(snout1.OffsetX, snout1.OffsetY, snout1.Height));
         var snout1CapCenterB = -0.5f * (new Vector3(snout1.OffsetX, snout1.OffsetY, snout1.Height));
-        (var snout1N, var snout1Dc) = GeometryHelper.GetPlaneFromShearAndPoint(
+        (var snout1_n, var snout1_dc) = GeometryHelper.GetPlaneFromShearAndPoint(
             snout1.TopShearX,
             snout1.TopShearY,
             snout1CapCenter
         );
-        var snout1N4D = new Vector4(snout1N.X, snout1N.Y, snout1N.Z, 0.0f);
+        var snout1_n_4d = new Vector4(snout1_n.X, snout1_n.Y, snout1_n.Z, 0.0f);
 
-        var distance10 = snout1N.X * ptOrigEll1[0] + snout1N.Y * ptOrigEll1[1] + snout1N.Z * ptOrigEll1[2] + snout1Dc;
-        var distance11 = snout1N.X * ptPt1Ell1[0] + snout1N.Y * ptPt1Ell1[1] + snout1N.Z * ptPt1Ell1[2] + snout1Dc;
-        var distance12 = snout1N.X * ptPt2Ell1[0] + snout1N.Y * ptPt2Ell1[1] + snout1N.Z * ptPt2Ell1[2] + snout1Dc;
+        var distance1_0 =
+            snout1_n.X * pt_orig_ell1[0] + snout1_n.Y * pt_orig_ell1[1] + snout1_n.Z * pt_orig_ell1[2] + snout1_dc;
+        var distance1_1 =
+            snout1_n.X * pt_pt1_ell1[0] + snout1_n.Y * pt_pt1_ell1[1] + snout1_n.Z * pt_pt1_ell1[2] + snout1_dc;
+        var distance1_2 =
+            snout1_n.X * pt_pt2_ell1[0] + snout1_n.Y * pt_pt2_ell1[1] + snout1_n.Z * pt_pt2_ell1[2] + snout1_dc;
 
         // snout2 -> bottom
         var snout2CapCenter = -0.5f * (new Vector3(snout2.OffsetX, snout2.OffsetY, snout2.Height));
-        (var snout2N, var snout2Dc) = GeometryHelper.GetPlaneFromShearAndPoint(
+        (var snout2_n, var snout2_dc) = GeometryHelper.GetPlaneFromShearAndPoint(
             snout2.BottomShearX,
             snout2.BottomShearY,
             snout2CapCenter
         );
-        var snout2N4D = new Vector4(snout2N.X, snout2N.Y, snout2N.Z, 0.0f);
+        var snout2_n_4d = new Vector4(snout2_n.X, snout2_n.Y, snout2_n.Z, 0.0f);
 
-        var distance20 = snout2N.X * ptOrigEll2[0] + snout2N.Y * ptOrigEll2[1] + snout2N.Z * ptOrigEll2[2] + snout2Dc;
-        var distance21 = snout2N.X * ptPt1Ell2[0] + snout2N.Y * ptPt1Ell2[1] + snout2N.Z * ptPt1Ell2[2] + snout2Dc;
-        var distance22 = snout2N.X * ptPt2Ell2[0] + snout2N.Y * ptPt2Ell2[1] + snout2N.Z * ptPt2Ell2[2] + snout2Dc;
+        var distance2_0 =
+            snout2_n.X * pt_orig_ell2[0] + snout2_n.Y * pt_orig_ell2[1] + snout2_n.Z * pt_orig_ell2[2] + snout2_dc;
+        var distance2_1 =
+            snout2_n.X * pt_pt1_ell2[0] + snout2_n.Y * pt_pt1_ell2[1] + snout2_n.Z * pt_pt1_ell2[2] + snout2_dc;
+        var distance2_2 =
+            snout2_n.X * pt_pt2_ell2[0] + snout2_n.Y * pt_pt2_ell2[1] + snout2_n.Z * pt_pt2_ell2[2] + snout2_dc;
 
-        var v4TransfNormal1 = Vector4.Transform(snout1N4D, snout1.Matrix);
-        var v4TransfNormal2 = Vector4.Transform(snout2N4D, snout2.Matrix);
-        var transfNormal1 = new Vector3(v4TransfNormal1.X, v4TransfNormal1.Y, v4TransfNormal1.Z);
-        var transfNormal2 = new Vector3(v4TransfNormal2.X, v4TransfNormal2.Y, v4TransfNormal2.Z);
+        var v4transfNormal1 = Vector4.Transform(snout1_n_4d, snout1.Matrix);
+        var v4transfNormal2 = Vector4.Transform(snout2_n_4d, snout2.Matrix);
+        var transfNormal1 = new Vector3(v4transfNormal1.X, v4transfNormal1.Y, v4transfNormal1.Z);
+        var transfNormal2 = new Vector3(v4transfNormal2.X, v4transfNormal2.Y, v4transfNormal2.Z);
         transfNormal1 = Vector3.Normalize(transfNormal1);
         transfNormal2 = Vector3.Normalize(transfNormal2);
 
@@ -1108,22 +1121,22 @@ public class CapVisibilityTests
         Matrix4x4 sn1MatInv;
         Matrix4x4.Invert(s1Mat, out sn1MatInv);
 
-        var p1W = Vector4.Transform(snout1CapCenter, s1Mat);
-        var p1M2 = Vector4.Transform(p1W, sn2MatInv);
-        var dC1Pl2 = Vector3.Dot(snout2N, new Vector3(p1M2.X, p1M2.Y, p1M2.Z)) + snout2Dc;
+        var p1_w = Vector4.Transform(snout1CapCenter, s1Mat);
+        var p1_m2 = Vector4.Transform(p1_w, sn2MatInv);
+        var d_c1_pl2 = Vector3.Dot(snout2_n, new Vector3(p1_m2.X, p1_m2.Y, p1_m2.Z)) + snout2_dc;
 
         var p2 = Vector4.Transform(Vector4.Transform(snout2CapCenter, s2Mat), sn1MatInv);
-        var dC2Pl1 = Vector3.Dot(snout1N, new Vector3(p2.X, p2.Y, p2.Z)) + snout1Dc;
+        var d_c2_pl1 = Vector3.Dot(snout1_n, new Vector3(p2.X, p2.Y, p2.Z)) + snout1_dc;
 
-        Assert.AreEqual(0.0, dC1Pl2);
-        Assert.AreEqual(0.0, dC2Pl1);
+        Assert.AreEqual(0.0, d_c1_pl2);
+        Assert.AreEqual(0.0, d_c2_pl1);
 
-        Assert.AreEqual(0.0, distance10 * 1000.0);
-        Assert.AreEqual(0.0, distance11 * 1000.0);
-        Assert.AreEqual(0.0, distance12 * 1000.0);
-        Assert.AreEqual(0.0, distance20 * 1000.0);
-        Assert.AreEqual(0.0, distance21 * 1000.0);
-        Assert.AreEqual(0.0, distance22 * 1000.0);
+        Assert.AreEqual(0.0, distance1_0 * 1000.0);
+        Assert.AreEqual(0.0, distance1_1 * 1000.0);
+        Assert.AreEqual(0.0, distance1_2 * 1000.0);
+        Assert.AreEqual(0.0, distance2_0 * 1000.0);
+        Assert.AreEqual(0.0, distance2_1 * 1000.0);
+        Assert.AreEqual(0.0, distance2_2 * 1000.0);
 
         snout1.Connections[0] = new RvmConnection(
             snout1,
@@ -1131,7 +1144,7 @@ public class CapVisibilityTests
             1,
             0,
             snout1CapCenter,
-            snout1N,
+            snout1_n,
             RvmConnection.ConnectionType.HasCircularSide
         );
 
@@ -1212,7 +1225,7 @@ public class CapVisibilityTests
         // snout1 -> top
         var snout1CapCenter = 0.5f * (new Vector3(snout1.OffsetX, snout1.OffsetY, snout1.Height));
         var snout1CapCenterB = -0.5f * (new Vector3(snout1.OffsetX, snout1.OffsetY, snout1.Height));
-        (var snout1N, _) = GeometryHelper.GetPlaneFromShearAndPoint(
+        (var snout1_n, _) = GeometryHelper.GetPlaneFromShearAndPoint(
             snout1.TopShearX,
             snout1.TopShearY,
             snout1CapCenter
@@ -1225,7 +1238,7 @@ public class CapVisibilityTests
             1,
             0,
             snout1CapCenter,
-            snout1N,
+            snout1_n,
             RvmConnection.ConnectionType.HasCircularSide
         );
 
@@ -1305,7 +1318,7 @@ public class CapVisibilityTests
         // snout1 -> top
         var snout1CapCenter = 0.5f * (new Vector3(snout1.OffsetX, snout1.OffsetY, snout1.Height));
         var snout1CapCenterB = -0.5f * (new Vector3(snout1.OffsetX, snout1.OffsetY, snout1.Height));
-        (var snout1N, _) = GeometryHelper.GetPlaneFromShearAndPoint(
+        (var snout1_n, _) = GeometryHelper.GetPlaneFromShearAndPoint(
             snout1.TopShearX,
             snout1.TopShearY,
             snout1CapCenter
@@ -1318,7 +1331,7 @@ public class CapVisibilityTests
             1,
             0,
             snout1CapCenter,
-            snout1N,
+            snout1_n,
             RvmConnection.ConnectionType.HasCircularSide
         );
 
@@ -1334,7 +1347,7 @@ public class CapVisibilityTests
         // snout2 -> bottom
         var snout2CapCenter = -0.5f * (new Vector3(snout2.OffsetX, snout2.OffsetY, snout2.Height));
         var snout2CapCenterB = 0.5f * (new Vector3(snout2.OffsetX, snout2.OffsetY, snout2.Height));
-        (var snout2N, _) = GeometryHelper.GetPlaneFromShearAndPoint(
+        (var snout2_n, _) = GeometryHelper.GetPlaneFromShearAndPoint(
             snout2.BottomShearX,
             snout2.BottomShearY,
             snout2CapCenter
@@ -1347,7 +1360,7 @@ public class CapVisibilityTests
             0,
             1,
             snout2CapCenter,
-            snout2N,
+            snout2_n,
             RvmConnection.ConnectionType.HasCircularSide
         );
 
