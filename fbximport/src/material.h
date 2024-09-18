8a440282d4ad7704a7385e04a367deac9daebb63
Stig Rune Søberg <ssob@equinor.com>
2024-09-18 13:36:10 +0200

    Fix indenting

--- fbximport/src/material.h
+++ fbximport/src/material.h
@@ -4,8 +4,8 @@
 #include "common.h"
 
 extern "C" {
-  CFBX_API void material_clean_memory(Color* color);
-	CFBX_API Color* material_get_color(CFbxMaterial material);
+    CFBX_API void material_clean_memory(Color* color);
+    CFBX_API Color* material_get_color(CFbxMaterial material);
 }
 
 #endif // __CFBX_MATERIAL_H__
