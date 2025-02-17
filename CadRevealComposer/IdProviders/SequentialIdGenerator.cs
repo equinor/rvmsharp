8902ff0d34413f4a4d0be9d81ba554d1df0dcfd6
Nils Henrik Hals <3185998+Strepto@users.noreply.github.com>
2025-02-17 17:01:37 +0100

    Update Id Generator error

--- CadRevealComposer/IdProviders/SequentialIdGenerator.cs
+++ CadRevealComposer/IdProviders/SequentialIdGenerator.cs
@@ -4,7 +4,7 @@ using System;
 
 public class SequentialIdGenerator(uint firstIdReturned = 0)
 {
-    private static readonly uint MaxSafeIdForReveal = (uint)Math.Pow(2, 24); // Max number of cells in a 4k texture (in reveal)
+    private static readonly uint MaxSafeIdForReveal = (uint)Math.Pow(2, 24); // Max number of cells in a 4k texture (and max sequential numbers in a float32)
 
     private uint _internalIdCounter = firstIdReturned; // It increments before selecting the id, hence -1
 
@@ -13,7 +13,7 @@ public class SequentialIdGenerator(uint firstIdReturned = 0)
         var idToReturn = _internalIdCounter;
         _internalIdCounter++;
         if (idToReturn > MaxSafeIdForReveal)
-            throw new Exception("Too many ids generated");
+            throw new Exception("Id overflow. Ids are not safe for reveal anymore. " + "Max is " + MaxSafeIdForReveal);
         return idToReturn;
     }
 
