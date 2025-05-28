e7cf6a5d0acc260059ce15305712e82903fa19d8
Nils Henrik Hals <3185998+Strepto@users.noreply.github.com>
2025-05-28 14:31:46 +0200

    Refactor and add comments

--- CadRevealComposer/IdProviders/SequentialIdGenerator.cs
+++ CadRevealComposer/IdProviders/SequentialIdGenerator.cs
@@ -2,17 +2,24 @@
 
 using System;
 
+/// <summary>
+/// Generates sequential IDs starting from a specified number.
+/// </summary>
 public class SequentialIdGenerator(uint firstIdReturned = 0)
 {
     private protected const uint MaxSafeIdForReveal = 16_777_216; // 2^24 -- Max number of cells in a 4k texture (and max sequential numbers in a float32)
 
     private uint _internalIdCounter = firstIdReturned;
 
-    private object IdLock { get; } = new object();
+    private readonly object _idLock = new object();
 
+    /// <summary>
+    /// Gets the next sequential ID, and increments the internal counter.
+    /// This is thread-safe and ensures that IDs are unique across calls.
+    /// </summary>
     public uint GetNextId()
     {
-        lock (IdLock)
+        lock (_idLock)
         {
             var idToReturn = _internalIdCounter;
             _internalIdCounter++;
@@ -23,13 +30,14 @@ public class SequentialIdGenerator(uint firstIdReturned = 0)
     }
 
     /// <summary>
-    /// Look at the next id that will be generated
+    /// Look at the next id that will be generated (without incrementing the counter).
+    /// Note: If you call this method in a multi-threaded environment, it will not guarantee that the ID will not change before you call GetNextId().
     /// </summary>
     public uint PeekNextId
     {
         get
         {
-            lock (IdLock)
+            lock (_idLock)
             {
                 return _internalIdCounter;
             }
