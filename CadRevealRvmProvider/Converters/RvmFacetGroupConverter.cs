23a6e3ffb0143c6c25bd9bb3baeba092c76b430f
Veronika S <ves@equinor.com>
2025-11-12 20:54:08 +0100

    disable warnings for unused variables

--- CadRevealRvmProvider/Converters/RvmFacetGroupConverter.cs
+++ CadRevealRvmProvider/Converters/RvmFacetGroupConverter.cs
@@ -17,15 +17,12 @@ public static class RvmFacetGroupConverter
         // code can be cleaned after the export of rvm models is properly fixed
         // for mode info see AB#255079
         var boundingBoxFromVertexCoords = rvmFacetGroup.CalculateBoundingBoxFromVertexPositions();
+        // ReSharper disable once UnusedVariable
         var boundingBoxFromRvmFile = rvmFacetGroup.BoundingBoxLocal;
-
+        // ReSharper disable once UnusedVariable
         var bbWorldFromRvmFile = rvmFacetGroup.CalculateAxisAlignedBoundingBox()!.ToCadRevealBoundingBox();
 
-        // When exporting RVM models is fixed, we should compare bbWorldFromRvmFileCancelRotation, bbWorldFromVertexPosition and bbWorldFromRvmFile
-        // Right now bbWorldFromVertexPosition ~~ bbWorldFromRvmFileCancelRotation, but it should be
-        // bbWorldFromVertexPosition ~~ bbWorldFromRvmFile instead
-        // bbWorldFromVertexPosition will then be removed and bbWorldFromRvmFile will be used in stead (for ProtoMeshFromFacetGroup)
-        // as bbWorldFromVertexPosition costs to compute and bbWorldFromRvmFile is for "free" (read from RVM file)
+        // ReSharper disable once UnusedVariable
         var bbWorldFromRvmFileCancelRotation = RvmBoundingBox
             .CalculateAxisAlignedBoundingBoxCancelRotation(rvmFacetGroup.BoundingBoxLocal, rvmFacetGroup.Matrix)!
             .ToCadRevealBoundingBox();
@@ -33,6 +30,11 @@ public static class RvmFacetGroupConverter
             .CalculateAxisAlignedBoundingBox(boundingBoxFromVertexCoords, rvmFacetGroup.Matrix)
             .ToCadRevealBoundingBox();
 
+        // When exporting RVM models is fixed, we should compare bbWorldFromRvmFileCancelRotation, bbWorldFromVertexPosition and bbWorldFromRvmFile
+        // Right now bbWorldFromVertexPosition ~~ bbWorldFromRvmFileCancelRotation, but it should be
+        // bbWorldFromVertexPosition ~~ bbWorldFromRvmFile instead
+        // bbWorldFromVertexPosition will then be removed and bbWorldFromRvmFile will be used in stead (for ProtoMeshFromFacetGroup)
+        // as bbWorldFromVertexPosition costs to compute and bbWorldFromRvmFile is for "free" (read from RVM file)
         yield return new ProtoMeshFromFacetGroup(rvmFacetGroup, treeIndex, color, bbWorldFromVertexPosition);
     }
 }
