d74d81c45ea992f0721b4cf543cdde36c13656c7
Veronika Solteszova <VES@equinor.com>
2026-04-30 10:38:33 +0200

    Add error message to custom exception when filename does not pass the check.

--- CadRevealFbxProvider/Attributes/ScaffoldingMetadata.cs
+++ CadRevealFbxProvider/Attributes/ScaffoldingMetadata.cs
@@ -238,7 +238,7 @@ public class ScaffoldingMetadata
         else
         {
             throw new UserFriendlyLogException(
-                "",
+                $"Scaffolding file's {filename} filename is not following the naming guide. Please check the naming guide.",
                 new ScaffoldingFilenameException(
                     $"Scaffolding file's {filename} filename is not following the naming guide. Please check the naming guide."
                 )
