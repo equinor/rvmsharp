31104e090c0d3e23c680962d3fa3928780477850
Veronika Solteszova <VES@equinor.com>
2026-04-30 10:55:31 +0200

    Add filename length constraint of 55 chars excl extension. Crash if filenames are too long.

--- CadRevealFbxProvider/BatchUtils/FbxWorkload.cs
+++ CadRevealFbxProvider/BatchUtils/FbxWorkload.cs
@@ -114,6 +114,18 @@ public static class FbxWorkload
             {
                 var lines = File.ReadAllLines(infoTextFilename);
                 var fileNameonly = Path.GetFileNameWithoutExtension(infoTextFilename);
+
+                // check the length of the filename. Upload artifacts will fail with too long filenames
+                if (fileNameonly.Length > 55)
+                {
+                    throw new UserFriendlyLogException(
+                        $"Scaffolding file's {fileNameonly} filename is exceeding the maximum allowed length of 55 characters. Check the naming guide.",
+                        new ScaffoldingFilenameException(
+                            $"Scaffolding file's {fileNameonly} filename is exceeding the maximum allowed length of 55 characters. Please check the naming guide."
+                        )
+                    );
+                }
+
                 var isTemp = fileNameonly.Contains("TEMP", StringComparison.OrdinalIgnoreCase);
 
                 (attributes, var scaffoldingMetadata) = ScaffoldingAttributeParser.ParseAttributes(lines, isTemp);
