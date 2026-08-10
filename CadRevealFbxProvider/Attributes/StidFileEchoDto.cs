fd91b4acb4b40bb5b026a81ec79ddc7d95b2688c
Veronika Solteszova <VES@equinor.com>
2026-08-10 15:29:56 +0200

    Reducing DTO fields to only those that are necessary

--- CadRevealFbxProvider/Attributes/StidFileEchoDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace CadRevealFbxProvider.Attributes
-{
-    /// This is a copy of the record with the same name from EchoModelDistribution project, which exports the json file
-    public record StidFileEchoDto
-    {
-        public required string BlobId { get; set; }
-        public required string FileName { get; set; }
-    }
-}
--- CadRevealFbxProvider/Attributes/StidScaffoldingDocumentEchoDto.cs
+++ CadRevealFbxProvider/Attributes/StidScaffoldingDocumentEchoDto.cs
@@ -2,55 +2,18 @@
 {
     /// <summary>
     /// Document metadata from STID with additional computed fields relevant for Echo
-    /// This is a copy of the record with the same name from EchoModelDistribution project, which exports the json file
+    /// This is a subset of the fields from the record with the same name from EchoModelDistribution project, which exports the json file
+    /// We keep only fields that are relevant for:
+    /// - cross checking the work order id,
+    /// - determining if the scaffolding has status TEMP and
+    /// - adding name suffix (document title can be used as a human-readable name)
     /// </summary>
     public record StidScaffoldingDocumentEchoDto
     {
         /// <summary>
-        /// The plant code associated with the document
-        /// Three letters in lower case, e.g. "abc"
-        /// </summary>
-        public required string PlantCode { get; set; }
-
-        /// <summary>
-        /// The installation code (JSV, JCA, TON, TROA, etc.)
-        /// </summary>
-        public required string InstCode { get; init; }
-
-        /// <summary>
-        /// The document number (document ID)
-        /// </summary>
-        public required string DocNo { get; init; }
-
-        /// <summary>
-        /// The document type (document type, like SCAFF)
-        /// </summary>
-        public required string DocType { get; init; }
-
-        /// <summary>
-        /// Title of the document
+        /// Title of the document, here used as suffix after the algorithmically created document name
         /// </summary>
         public required string DocTitle { get; init; }
-
-        /// <summary>
-        /// The document classification code
-        /// </summary>
-        public required string DocClass { get; init; }
-
-        /// <summary>
-        /// Optional remark on the document
-        /// </summary>
-        /// <example>
-        /// "Validation failed due to missing work order reference". Or similar...
-        /// </example>
-        public string? Remark { get; init; }
-
-        public required string RevNo { get; set; }
-
-        /// <summary>
-        /// Additional fields, e.g., validation, work order
-        /// </summary>
-        public required StidFileEchoDto[] Files { get; init; } = [];
 
         /// <summary>
         /// WorkOrderId without zero padding.
@@ -62,12 +25,5 @@
         /// When a scaffolding does not have a work order assigned we assume it is in planning phase and simplify some of the guards and checks on the scaffolding files.
         /// </summary>
         public required bool HasWorkOrderAssigned { get; set; } = false;
-
-        /// <summary>
-        /// This is a simplified status to easily check if the document has passed the validation checks.
-        /// </summary>
-        public required bool HasBeenValidatedSuccessfully { get; set; } = false;
-
-        public required string PlantDocNo { get; set; }
     }
 }
